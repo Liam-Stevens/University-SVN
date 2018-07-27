9e2c200eb0b660ad1e471b4f9605fd3fe2ce409d
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-07-27 05:41:57 +0000

    Q1.3 Working

--- 2018/s2/oop/practical-01/main-1-3.cpp
+++ 2018/s2/oop/practical-01/main-1-3.cpp
@@ -7,7 +7,7 @@ int main(int argc,char **argv)
 {
 	int array[6] = {1,2,3,4,5,6};
 	int num = count(array, 6, 6);
-	std:cout << "There are " << num << "six(es)" << std::endl;
+	std::cout << "There are " << num << " six(es)" << std::endl;
 	return 0 ;
 }
 
