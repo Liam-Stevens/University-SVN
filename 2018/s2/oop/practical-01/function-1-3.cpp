578359d3aaf0941624d2bfca3e68bc8b4b072ecf
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-07-27 05:39:56 +0000

    Added Q1.3

---
+++ 2018/s2/oop/practical-01/function-1-3.cpp
@@ -0,0 +1,15 @@
+// function to count a certain num in array
+int count(int array[], int n, int num)
+{
+	if(n < 1){
+		return 0;
+	} else {
+		int count = 0;
+		for(int i = 0; i < n; i++){
+			if(array[i] == num) {
+				count++;
+			}
+		}
+		return count++;
+	}
+}
---
+++ 2018/s2/oop/practical-01/main-1-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+
+extern int count(int*,int,int);
+
+int main(int argc,char **argv)
+{
+	int array[6] = {1,2,3,4,5,6};
+	int num = count(array, 6, 6);
+	std:cout << "There are " << num << "six(es)" << std::endl;
+	return 0 ;
+}
+
