f4598cfd885ba54624a3dcb363db7670926c81b2
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-07-27 05:58:27 +0000

    Q1.4 working

--- 2018/s2/oop/practical-01/function-1-4.cpp
+++ 2018/s2/oop/practical-01/function-1-4.cpp
@@ -1,13 +1,14 @@
 // function to sum two arrays
 int sumtwo(int array[], int secondarray[], int n)
 {
-	/if(n < 1){
+	if(n < 1){
 		return 0;
 	} else {
 		int sum = 0;
 		for(int i = 0; i < n; i++){
 			sum = sum + array[i];
-			sum = sum + secondaryarray[i];
+			sum = sum + secondarray[i];
 		}
+		return sum;
 	}
 }
