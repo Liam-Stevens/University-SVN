67837036048b410cd3afcb57f6d43411ae75e6f6
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-07-30 06:15:11 +0000

    We wrote a program to perform binary arithmetic

---
+++ 2018/s2/oop/workshop-02/arithmetic.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int binary[] = {0,1,1,0,1,0,1,1};
+	int temp[] = {0,0,0,0,0,0,0,0};
+
+	for (int i=0; i < 8; i++)
+	{
+		if(i != 0) 
+		{
+			temp[i] = binary[i-1];
+		} else {
+			temp[i] = binary[7];
+		}
+	}
+
+	for (int i=0; i < 8; i++)
+	{	
+		binary[i] = temp[i];
+		std::cout << binary[i];
+	}
+	std::cout << std::endl;
+	return 0;
+}
