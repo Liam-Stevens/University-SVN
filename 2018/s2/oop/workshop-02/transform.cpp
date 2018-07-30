b87fcb69ccfbab2eb6c1b6a6605276412a7027e5
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-07-30 05:39:46 +0000

    We wrote a program to generate binary numbers

--- 2018/s2/oop/workshop-02/transform.cpp
+++ 2018/s2/oop/workshop-02/transform.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <cmath>
 
-void main() {
+int main() 
+{
 	int value = 158;
 	int binary[] = {0,0,0,0,0,0,0,0};
 
-	for(int i=0; i < 8; i++){
+	for(int i = 0; i < 8; i++){
+
 		binary[i] = value / pow(2,(7-i));
-		if(binary[i] == 1) {
+		if(binary[i] == 1) 
+		{
 			value = value - pow(2,(7-i));
 		}
+
+		std::cout << binary[i];
 	}
-	std::cout <<
+	
+	std::cout << std::endl;
+	return 0;
 }
