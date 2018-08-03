01a4e884cd4a452c821f9cea050b4a473a7caf29
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-08-03 06:12:23 +0000

    Question 2-1 Real

---
+++ 2018/s2/oop/practical-02/function-2-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+
+void print_as_binary(std::string decimal_number)
+{
+	int num = atoi(decimal_number.c_str());
+
+	for (int i = 63; i > -1; i--)
+	{
+		if (num/(pow(2,i)) > 1)
+		{
+			num = num - pow(2,i);
+			std::cout << "1";
+		}
+		else
+		{
+			std::cout << "0";
+		}
+	}
+
+	std::cout << std::endl;
+}
---
+++ 2018/s2/oop/practical-02/main-2-1.cpp
@@ -0,0 +1,8 @@
+#include <string>
+extern void print_as_binary(std::string);
+
+int main() 
+{
+	print_as_binary("43");
+	return 0;
+}
