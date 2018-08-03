bf848536732c92e3b0d9f4965e1fb6a32c6d1655
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-08-03 06:15:34 +0000

    Question 2-1 Fix 2

--- 2018/s2/oop/practical-02/function-2-1.cpp
+++ 2018/s2/oop/practical-02/function-2-1.cpp
@@ -10,7 +10,7 @@ void print_as_binary(std::string decimal_number)
 
 	for (int i = 63; i > -1; i--)
 	{
-		if (num/(pow(2,i)) > 1)
+		if (num/(pow(2,i)) >= 1)
 		{
 			num = num - pow(2,i);
 			std::cout << "1";
