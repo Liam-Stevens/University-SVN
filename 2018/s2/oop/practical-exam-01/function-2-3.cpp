570ef451456fbd56b1fcdc6c03f9238f764d7fc2
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-08-10 05:39:12 +0000

    Added Commenting

--- 2018/s2/oop/practical-exam-01/function-2-3.cpp
+++ 2018/s2/oop/practical-exam-01/function-2-3.cpp
@@ -2,15 +2,18 @@
 
 int multiply_if_threes(int integers[], int length)
 {
+	//check if length is zero or negative
 	if (length <= 0)
 	{
 		return -1;
 	} 
 	else 
 	{
+		//allows for a non-zero starting multiple
 		int product = integers[0];
 		for (int i = 1; i < length; i++)
 		{
+			//will iterate over entire array and multiple every element together
 			product = product*integers[i];
 		}
 		return product;
@@ -19,12 +22,14 @@ int multiply_if_threes(int integers[], int length)
 
 bool is_ascending_multiple_threes(int integers[], int length)
 {
+	//check if length is zero or negative
 	if (length <= 0)
 	{
 		return false;
 	}
 	else
 	{
+		//checks if the first integer is a multiple of three
 		if (integers[0] % 3 != 0)
 		{
 			return false;
@@ -32,6 +37,8 @@ bool is_ascending_multiple_threes(int integers[], int length)
 
 		for (int i = 1; i < length; i++)
 		{
+			//checks if each integer in the array is a multiple of three
+			//in reference to the first integer (which checks ascending order aswell)
 			if(integers[i] != integers[0] + i*3)
 			{
 				return false;
@@ -43,15 +50,18 @@ bool is_ascending_multiple_threes(int integers[], int length)
 
 int multiply_elements(int integers[], int length)
 {
+	//check if length is zero or negative
 	if (length <= 0)
 	{
 		return -1;
 	}
 	else
 	{
+		//allows for a non-zero starting multiple
 		int product = integers[0];
 		for (int i = 1; i < length; i++)
 		{
+			//will iterate over entire array and multiple every element together
 			product = product*integers[i];
 		}
 		return product;
