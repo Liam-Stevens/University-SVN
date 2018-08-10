f4ddfb993908c5b8b08828e690a313248f445739
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-08-10 04:53:56 +0000

    Prac Ex Q2-2 fix

--- 2018/s2/oop/practical-exam-01/function-2-2.cpp
+++ 2018/s2/oop/practical-exam-01/function-2-2.cpp
@@ -5,7 +5,7 @@ int octal_to_decimal(int octal_digits[], int number_of_digits)
 	int decimal = 0;
 	for (int i = 0; i < number_of_digits; i++)
 	{
-		decimal = decimal + octal_digits[i]*pow(8,(number_of_digits-i));
+		decimal = decimal + octal_digits[i]*pow(8,(number_of_digits-1-i));
 	}
 	return decimal;
 }
