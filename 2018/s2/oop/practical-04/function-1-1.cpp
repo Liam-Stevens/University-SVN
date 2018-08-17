09103bd7608b7267586b066db92fdec15e256683
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-08-17 05:03:09 +0000

    Q1-1 fix

--- 2018/s2/oop/practical-04/function-1-1.cpp
+++ 2018/s2/oop/practical-04/function-1-1.cpp
@@ -8,8 +8,6 @@ void copy_2d_strings(std::string first[][2], std::string second[][2], int rows)
 		for (int j = 0; j < 2; j++)
 		{
 			second[i][j] = first[i][j];
-			std::cout << second[i][j] << " ";
 		}
-		std::cout << std::endl;
 	}
 }
