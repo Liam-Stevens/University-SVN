c110f991117634809f3be4f3d3be01af44ca2646
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-08-20 05:35:59 +0000

    W5 Q3-1

--- 2018/s2/oop/workshop-05/function-3-1.cpp
+++ 2018/s2/oop/workshop-05/function-3-1.cpp
@@ -57,3 +57,22 @@ void hexDigits(int *numbers,int length)
 	}
 
 }
+
+bool equalsArray(int *numbers1,int *numbers2,int length) 
+{
+	if (length < 1)
+	{
+		return false;
+	}
+	else
+	{
+		for (int i = 0; i < length; i++)
+		{
+			if ( *(numbers1 + i) != *(numbers2 + i))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
