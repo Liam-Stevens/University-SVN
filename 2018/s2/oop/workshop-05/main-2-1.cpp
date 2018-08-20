2743e55e7792e1f48473ad5652e91e99f5a2b914
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-08-20 05:24:57 +0000

    W5 Q2-1 fix

--- 2018/s2/oop/workshop-05/main-2-1.cpp
+++ 2018/s2/oop/workshop-05/main-2-1.cpp
@@ -1,9 +1,11 @@
 extern void hexDigits(int *numbers,int length) ;
+extern int *readNumbers() ;
+extern void printNumbers(int *numbers,int length) ;
 
 int main()
 {
 	int array1[10] = {11,12,3,4,15,6,7,8,9,10};
-	int *test = &array1[0];
+	int *test = readNumbers();
 	hexDigits(test, 10);
 	return 0;
 }
