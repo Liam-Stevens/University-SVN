8efd764932f4c2fd28764401f8280a625b107bd0
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-08-20 05:24:57 +0000

    W5 Q2-1 fix

--- 2018/s2/oop/workshop-05/function-2-1.cpp
+++ 2018/s2/oop/workshop-05/function-2-1.cpp
@@ -2,6 +2,31 @@
 #include <string>
 using namespace std;
 
+#include <iostream>
+
+int *readNumbers() 
+{
+	int *j = new int[10];
+	for(int i = 0; i < 10; i++)
+	{
+		std::cin >> *(j+i);
+	}
+	return j;
+}
+
+void printNumbers(int *numbers,int length) 
+{
+	for(int i = 0; i < length; i++)
+	{
+		std::cout << i << " " << *(numbers + i) << std::endl;
+	}
+
+	for(int j = 0; j < length; j++)
+	{
+		*(numbers + j) = 0;
+	}
+}
+
 void hexDigits(int *numbers,int length) 
 {
 	for(int i = 0; i < length; i++)
