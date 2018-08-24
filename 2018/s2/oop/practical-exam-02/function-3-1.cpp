59a820a567a0098e8e2a59ba1a73bc846d6bcabf
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-08-24 05:10:08 +0000

    Ex2 Q3-1

---
+++ 2018/s2/oop/practical-exam-02/function-3-1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int * readNumbers(int n) 
+{
+	int *j = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> *(j + i);
+	}
+	return j;
+}
+
+void printNumbers(int * numbers, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		cout << i << " " << *(numbers + i) << endl;
+	}
+}
+
+int *swapFirstLast(int * numbers1, int length)
+{
+	int *j = new int[length];
+	for (int i = 0; i < length; i++)
+	{
+		*(j + i) = *(numbers1 + length - 1 - i);
+	}
+	return j;
+}
