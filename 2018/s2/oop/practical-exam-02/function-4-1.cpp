2f64a01b6e78a6e9f522359797fa740a21510d45
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-08-24 05:47:45 +0000

    fix 3

--- 2018/s2/oop/practical-exam-02/function-4-1.cpp
+++ 2018/s2/oop/practical-exam-02/function-4-1.cpp
@@ -3,19 +3,31 @@ using namespace std;
 
 int * readNumbers(int n) 
 {
+	//Creates dynamic array
 	int *j = new int[n];
+
+	//Iterates over array, inputting for each element
 	for (int i = 0; i < n; i++)
 	{
 		cin >> *(j + i);
 	}
+
+	//Return the address of the start of the array
 	return j;
 }
 
 void printNumbers(int * numbers, int length)
 {
+	//Iterates over array, printing each element and element number
 	for (int i = 0; i < length; i++)
 	{
 		cout << i << " " << *(numbers + i) << endl;
+	}
+
+	//Deletes numbers in array
+	for (int i = 0; i < length; i++)
+	{
+		*(numbers + i) = 0;
 	}
 }
 
@@ -40,7 +52,7 @@ double meanSubArrays(int * numbers, int length)
 			count++;
 		}
 		//Clears value of sub array when starting new position
-		test = 0;
+		
 	}
 
 	//Calculates and prints mean
