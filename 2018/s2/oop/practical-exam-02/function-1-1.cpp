a722d684eb33fa0eae1061b1c125a3658165bbe1
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-08-24 05:45:35 +0000

    Comments Added

--- 2018/s2/oop/practical-exam-02/function-1-1.cpp
+++ 2018/s2/oop/practical-exam-02/function-1-1.cpp
@@ -3,18 +3,30 @@ using namespace std;
 
 int * readNumbers(int n) 
 {
+	//Creates dynamic array
 	int *j = new int[n];
+
+	//Iterates over array, inputting for each element
 	for (int i = 0; i < n; i++)
 	{
 		cin >> *(j + i);
 	}
+
+	//Return the address of the start of the array
 	return j;
 }
 
 void printNumbers(int * numbers, int length)
 {
+	//Iterates over array, printing each element and element number
 	for (int i = 0; i < length; i++)
 	{
 		cout << i << " " << *(numbers + i) << endl;
+	}
+
+	//Deletes numbers in array
+	for (int i = 0; i < length; i++)
+	{
+		*(numbers + i) = 0;
 	}
 }
--- 2018/s2/oop/practical-exam-02/function-3-1.cpp
+++ 2018/s2/oop/practical-exam-02/function-3-1.cpp
@@ -3,21 +3,28 @@ using namespace std;
 
 int * readNumbers(int n) 
 {
+	//Creates dynamic array
 	int *j = new int[n];
+
+	//Iterates over array, inputting for each element
 	for (int i = 0; i < n; i++)
 	{
 		cin >> *(j + i);
 	}
+
+	//Return the address of the start of the array
 	return j;
 }
 
 void printNumbers(int * numbers, int length)
 {
+	//Iterates over array, printing each element and element number
 	for (int i = 0; i < length; i++)
 	{
 		cout << i << " " << *(numbers + i) << endl;
 	}
 
+	//Deletes numbers in array
 	for (int i = 0; i < length; i++)
 	{
 		*(numbers + i) = 0;
@@ -26,16 +33,21 @@ void printNumbers(int * numbers, int length)
 
 int *swapFirstLast(int *numbers1, int length)
 {
+	//Creates dynamic array
 	int *j = new int[length];
+
+	//Sets the first number of new array to be last number of old array, iterating accross both ways
 	for (int i = 0; i < length; i++)
 	{
 		*(j + i) = *(numbers1 + (length - 1) - i);
 	}
 
+	//Deletes numbers in array
 	for (int i = 0; i < length; i++)
 	{
 		*(numbers1 + i) = 0;
 	}
 
+	//Returns starting address of new array
 	return j;
 }
