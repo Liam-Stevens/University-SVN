fc81b955f6ca1645460f5162b8d76f53eb924b6f
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-08-27 04:38:21 +0000

    directory

--- 2018/s2/oop/practical-04/main-2-2.cpp
+++ 2018/s2/oop/practical-04/main-2-2.cpp
@@ -2,7 +2,7 @@ extern int maximum_sum(int *,int);
 
 int main()
 {
-	int array1[10] = { -31, -41, -59, -26, -53, -58, -97, -93, -23, -84 };
+	int array1[10] = { -31, -40, -59, -26, -53, -58, -97, -93, -23, -84 };
 	int *ptr = &array1[0];
 	maximum_sum(ptr,10);
 
