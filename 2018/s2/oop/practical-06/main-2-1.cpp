839e57b2b6dcf13846d25dff529dab84de63fe2d
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-08-31 04:17:58 +0000

    go to uni

---
+++ 2018/s2/oop/practical-06/main-2-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "cart.h"
+#include "meerkat.h"
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  cart cart1;
+
+  bool addMeerkat(meerkat meer1);
+  bool addMeerkat(meerkat meer2);
+  bool addMeerkat(meerkat meer3);
+  bool addMeerkat(meerkat meer4);
+  bool addMeerkat(meerkat meer5);
+
+  return 0;
+}
