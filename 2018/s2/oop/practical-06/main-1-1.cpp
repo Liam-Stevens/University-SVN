66e474dc94acc02a7824318338b5b586ec19765e
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-08-31 02:42:11 +0000

    Works?

--- 2018/s2/oop/practical-06/main-1-1.cpp
+++ 2018/s2/oop/practical-06/main-1-1.cpp
@@ -1,42 +1,43 @@
-#include <string>
+#include <iostream>
 #include "meerkat.h"
 
 using namespace std;
 
-int main
+// main program
+int main()
 {
-	Meerkat meer1;
-	Meerkat meer2;
-	Meerkat meer3;
-	Meerkat meer4;
+  Meerkat meer1;
+  Meerkat meer2;
+  Meerkat meer3;
+  Meerkat meer4;
 
-	meer1.getName();
-	meer1.getAge();
-	meer1.setName("John");
-	meer1.setAge(21);
-	meer1.getName();
-	meer1.getAge();
+  meer1.getName();
+  meer1.getAge();
+  meer1.setName("John");
+  meer1.setAge(21);
+  meer1.getName();
+  meer1.getAge();
 
-	meer2.getName();
-	meer2.getAge();
-	meer2.setName("Steve");
-	meer2.setAge(12);
-	meer2.getName();
-	meer2.getAge();
+  meer2.getName();
+  meer2.getAge();
+  meer2.setName("Steve");
+  meer2.setAge(12);
+  meer2.getName();
+  meer2.getAge();
 
-	meer3.getName();
-	meer3.getAge();
-	meer3.setName("Kevin");
-	meer3.setAge(91);
-	meer3.getName();
-	meer3.getAge();
+  meer3.getName();
+  meer3.getAge();
+  meer3.setName("Kevin");
+  meer3.setAge(91);
+  meer3.getName();
+  meer3.getAge();
 
-	meer4.getName();
-	meer4.getAge();
-	meer4.setName("Nevil");
-	meer4.setAge(2);
-	meer4.getName();
-	meer4.getAge();
+  meer4.getName();
+  meer4.getAge();
+  meer4.setName("Nevil");
+  meer4.setAge(2);
+  meer4.getName();
+  meer4.getAge();
 
-	return 0;
+  return 0;
 }
