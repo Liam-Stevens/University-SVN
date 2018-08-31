47fcb1a98de6581c2122012b6818e0eba2cfc79c
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-08-31 02:59:06 +0000

    Plz444

--- 2018/s2/oop/practical-06/main-1-1.cpp
+++ 2018/s2/oop/practical-06/main-1-1.cpp
@@ -11,21 +11,29 @@ int main()
   meerkat meer3;
   meerkat meer4;
 
+  meer1.getName();
+  meer1.getAge();
   meer1.setName("John");
   meer1.setAge(21);
   meer1.getName();
   meer1.getAge();
 
+  meer2.getName();
+  meer2.getAge();
   meer2.setName("Steve");
   meer2.setAge(12);
   meer2.getName();
   meer2.getAge();
 
+  meer3.getName();
+  meer3.getAge();
   meer3.setName("Kevin");
   meer3.setAge(91);
   meer3.getName();
   meer3.getAge();
 
+  meer4.getName();
+  meer4.getAge();
   meer4.setName("Nevil");
   meer4.setAge(2);
   meer4.getName();
