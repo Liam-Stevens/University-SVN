d18f831120d22f34b97bd567ed503e7086d85906
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-09-07 05:45:38 +0000

    Comments

--- 2018/s2/oop/practical-exam-03/main-1-1.cpp
+++ 2018/s2/oop/practical-exam-03/main-1-1.cpp
@@ -3,8 +3,11 @@
 
 int main()
 {
+	//Player creation
 	player player1();
 	player player2(21,"John");
+
+	//Gets player2 information
 	player2.get_name();
 	player2.get_jersey_number();
 
--- 2018/s2/oop/practical-exam-03/main-1-2.cpp
+++ 2018/s2/oop/practical-exam-03/main-1-2.cpp
@@ -7,15 +7,19 @@ using namespace std;
 
 int main()
 {
+	//Creates player to be put in team
 	player player1(3,"John");
 
+	//Creates teams
 	team team1();
 	team team2(5, "Falcon");
 
+	//Gets information on team2
 	team2.get_current_number_of_players();
 	team2.get_name();
 	team2.get_roster();
 
+	//Adds player1 to team2
 	team2.add_player(player1);
 
 	return 0;
