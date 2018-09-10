f44736af1bd473af31e9f6db13cd202035584546
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-09-10 05:34:24 +0000

    Major Prac

--- 2018/s2/oop/workshop-08/main-3-1.cpp
+++ 2018/s2/oop/workshop-08/main-3-1.cpp
@@ -14,15 +14,6 @@ int main()
 	vegie vegie1("Cow");
 	vegie vegie2("Cow");
 
-/*
-	animal * ptr1 = &hunter1;
-	animal * ptr2 = &hunter2;
-	animal * ptr3 = &hunter3;
-	animal * ptr4 = &vegie1;
-	animal * ptr5 = &vegie2;
-	*/
-
-	//int collection[5] = {ptrh1,ptrh2,ptrh3,ptrv1,ptrv2};
 	animal** container = new animal*[5];
 
 	container[0] = &hunter1;
@@ -31,12 +22,7 @@ int main()
 	container[3] = &vegie1;
 	container[4] = &vegie2;
 
-/*
-	for(int i = 0; i < 5; i++)
-	{
-		container[i] = ptr(i);
-	}
-	*/
+	delete[] container;
 
 	return 0;
 }
