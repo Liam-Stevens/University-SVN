1e7db0c6819996dbe348d9300300672f60107e70
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-10-03 08:37:10 +0000

    group project progress 1

---
+++ 2018/s2/oop/group-project/player.cpp
@@ -0,0 +1,23 @@
+#include "player.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int player::count = 0;
+
+
+player::player()
+{
+
+}
+
+player::player()
+{
+
+}
+
+player::~player()
+{
+
+}
