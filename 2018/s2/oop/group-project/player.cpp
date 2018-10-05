7bac377edf032a25fc096509d61ac8f997861f28
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-10-05 04:14:20 +0000

    Before PracW9 work

--- 2018/s2/oop/group-project/player.cpp
+++ 2018/s2/oop/group-project/player.cpp
@@ -4,17 +4,38 @@
 
 using namespace std;
 
-int player::count = 0;
-
 
 player::player()
 {
-
+	money = 500;
 }
 
-player::player()
+player::player(double createMoney)
 {
+	money = createMoney;
+}
 
+//getters
+player::getMoney()
+{
+	return money;
+}
+
+//setters
+player::setMoney(double newMoney)
+{
+	money = newMoney;
+}
+
+//useful functions
+player::reduceMoney(double reduceMoneyBy)
+{
+	money -= reduceMoneyBy;
+}
+
+player::increaseMoney(double increaseMoneyBy)
+{
+	money += increaseMoneyBy;
 }
 
 player::~player()
--- 2018/s2/oop/group-project/stall.h
+++ 2018/s2/oop/group-project/stall.h
@@ -6,13 +6,31 @@
 class stall
 {
   static int count;
+  int stallID;
   std::string name;
+  std::string shopType
   double money;
   //Arrays of item, their price and quantity avaliable
 
  public:
 	stall();
-	stall(std::string name, );
+	stall(std::string createName, std::string createShopType, double createMoney);
+
+	//getters
+	getName();
+	getType();
+	getMoney();
+
+	//setters
+	setName(std::string newName);
+	setType(std::string newType);
+	setMoney(double newMoney);
+
+	//usable functions
+	reduceMoney(double reduceMoneyBy);
+	increaseMoney(double increaseMoneyBy);
+
+	//Need functions for adjusting shop stock
 
 	~stall();
 };
