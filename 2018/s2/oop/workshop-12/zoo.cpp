07cd6534aab9a84f43dd948e91018c5eeacef814
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-10-22 05:32:03 +0000

    Q3 PLEASE

--- 2018/s2/oop/workshop-12/zoo.cpp
+++ 2018/s2/oop/workshop-12/zoo.cpp
@@ -9,25 +9,24 @@ using namespace std;
 zoo::zoo(std::string n,int cows,int lions)
 {
 	name = n;
-	if (cows > 0 || lions > 0)
+	if (cows > 0 && lions > 0)
 	{
 		number_of_animals = cows + lions;
 		animals = new animal*[number_of_animals];
 
 		for(int i = 0; i < cows; i++)
 		{
-			vegie myCow("Daisy",100);
-			**(animals + i) = myCow;
+			*(animals + i) = new vegie("Daisy",100);
 		}
 		for(int i = cows; i < number_of_animals; i++)
 		{
-			hunter myLion("Clarence",50);
-			**(animals + i) = myLion;
+			*(animals + i) = new hunter("Clarence",50);
 		}
 	}
 	else
 	{
 		number_of_animals = 0;
+		animals = NULL;
 	}
 }
 
@@ -48,5 +47,8 @@ animal ** zoo::get_animals()
 
 zoo::~zoo()
 {
-	delete[] animals;
+	if (animals != NULL)
+	{
+		delete[] animals;
+	}
 }
