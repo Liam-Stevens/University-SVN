cdb72dc5079a58ce447ee3a1236861154d2b4ce1
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-10-22 05:10:16 +0000

    Q3 WORKPLZ2

--- 2018/s2/oop/workshop-12/zoo.cpp
+++ 2018/s2/oop/workshop-12/zoo.cpp
@@ -9,18 +9,21 @@ using namespace std;
 zoo::zoo(std::string n,int cows,int lions)
 {
 	name = n;
-	number_of_animals = cows + lions;
-	animals = new animal*[number_of_animals];
+	if (cow || lions < 0)
+	{
+		number_of_animals = cows + lions;
+		animals[*] = new animal*[number_of_animals];
 
-	for(int i = 0; i < cows; i++)
-	{
-		vegie myCow("Daisy",100);
-		**(animals + i) = myCow;
-	}
-	for(int i = cows; i < number_of_animals; i++)
-	{
-		hunter myLion("Clarence",50);
-		**(animals + i) = myLion;
+		for(int i = 0; i < cows; i++)
+		{
+			vegie myCow("Daisy",100);
+			**(animals + i) = myCow;
+		}
+		for(int i = cows; i < number_of_animals; i++)
+		{
+			hunter myLion("Clarence",50);
+			**(animals + i) = myLion;
+		}
 	}
 }
 
