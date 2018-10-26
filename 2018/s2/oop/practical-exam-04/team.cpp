ddbe6c7c9b3362dbbed80a633ec81bc65596aa6e
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-10-26 04:45:07 +0000

    Q3-1 A1

---
+++ 2018/s2/oop/practical-exam-04/team.cpp
@@ -0,0 +1,28 @@
+#include "person.h"
+#include "coach.h"
+#include "player.h"
+#include "team.h"
+#include <string>
+
+team::team()
+{
+	teamPlayers*[0] = new coach();
+	for(int i = 1; i < 4; i++)
+	{
+		teamPlayers*[i] = new player();
+	}
+}
+
+person ** team::getTeam()
+{
+	return teamPlayers*[0];
+}
+
+team::~team()
+{
+	for(int i = 0; i < 4; i++)
+	{
+		delete teamPlayers*[i];
+	}
+	delete[] teamPlayers;
+}
