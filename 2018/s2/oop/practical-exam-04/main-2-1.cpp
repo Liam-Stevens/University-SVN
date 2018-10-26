6f36cd1ef98110c8bffca56fbddaf082b8faabc8
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2018-10-26 04:12:40 +0000

    Q2-1 A2

--- 2018/s2/oop/practical-exam-04/main-2-1.cpp
+++ 2018/s2/oop/practical-exam-04/main-2-1.cpp
@@ -1,10 +1,18 @@
 #include "person.h"
 #include "coach.h"
 #include <string>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
-	coach james("James", 10);
+	coach james("James", 1);
+	coach james2("James", 1);
+	coach james3("James", 1);
+	james.set_salary(20);
+	cout << james.get_salary() << endl;
+	cout << james3.get_personID() << endl;
 
 	return 0;
 }
