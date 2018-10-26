9dc043642955aacaea20df5d505d89ac9ade35c6
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2018-10-26 05:22:34 +0000

    Q4-3 A2

--- 2018/s2/oop/practical-exam-04/sort_person.cpp
+++ 2018/s2/oop/practical-exam-04/sort_person.cpp
@@ -1,7 +1,24 @@
-#include "people.h"
+#include "person.h"
 #include "sort_person.h"
 
 void sort_person::sort(person **people, int n)
 {
+	int temp;
 
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			//int low = (people[i].get_personID() + i);
+			//int high = (people[i].get_personID() + i - 1 + j);
+			//if (low < high)
+			//{
+				//temp = high;
+				//high = low;
+				//low = temp;
+				//*(people.get_personID + i - 1 + j) = low;
+				//*(people.get_personID + i) = high
+			//}
+		}
+	}
 }
