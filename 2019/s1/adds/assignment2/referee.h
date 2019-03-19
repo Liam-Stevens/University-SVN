86ed231e5726b46b287a4bfdd0a47b6ada81fe74
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-03-19 04:44:00 +0000

    not working version

---
+++ 2019/s1/adds/assignment2/referee.h
@@ -0,0 +1,24 @@
+#ifndef REFEREE_H
+#define REFEREE_H
+
+#include <string>
+
+class referee
+{
+protected:
+	std::string * strat1;
+	std::string * strat2;
+	int games;
+	std::string * results;
+
+public:
+	
+	referee(std::string * newStrat1, std::string * newStrat2, int newGames);
+
+	void compare();
+	void printResults();
+
+	~referee();
+};
+
+#endif
