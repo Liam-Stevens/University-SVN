afac29b930cdf539cf6b1912f842aba772e92be6
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-03-22 04:33:29 +0000

    Design Added

--- 2019/s1/adds/assignment2/referee.cpp
+++ 2019/s1/adds/assignment2/referee.cpp
@@ -13,7 +13,6 @@ referee::referee(std::string * newStrat1, std::string * newStrat2, int newGames)
 
 void referee::compare()
 {
-	//results = new char[2*games];
 	for (int i = 0; i < games; i++)
 	{
 		//Check if the strategy is the same
@@ -30,13 +29,11 @@ void referee::compare()
 		else if ((strat1[i] == "R" && strat2[i] == "P") || (strat1[i] == "P" && strat2[i] == "S") || (strat1[i] == "S" && strat2[i] == "R"))
 		{
 			results.push_back('L');
-			//results[2*i] = 'L';
 		}
 		//Places a space between each letter
 		if(i != games-1)
 		{
 			results.push_back(' ');
-			//results[2*i-1] = ' ';
 		}
 	}
 
@@ -45,22 +42,10 @@ void referee::compare()
 void referee::printResults()
 {
 	//Prints the wins vs losses to the console
-	//string winner = "T W L";
-	/*
-	for (int i = 0; i < 2*games; i++)
-	{
-		if(results[i] == winner[i])
-		{
-			cout << "#" << i << endl;
-		}
-	}
-	*/
-	//cout << "#";
 	for (int i = 0; i < 2*games-1; i++)
 	{
 		cout << results[i];
 	}
-	//cout << "#";
 	cout << endl;
 }
 
