0b80c5c26f636995a13918e40e0bc1f640bd5424
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-03-22 01:20:58 +0000

    PLZ FIX REFEREE

--- 2019/s1/adds/assignment2/referee.cpp
+++ 2019/s1/adds/assignment2/referee.cpp
@@ -13,49 +13,52 @@ referee::referee(std::string * newStrat1, std::string * newStrat2, int newGames)
 
 void referee::compare()
 {
-	results = new char[2*games];
+	//results = new char[2*games];
 	for (int i = 0; i < games; i++)
 	{
 		//Check if the strategy is the same
 		if (strat1[i] == strat2[i])
 		{
-			results[2*i] = 'T';
+			results.push_back('T');
 		}
 		//Checks if player 1 beat player 2
 		else if ((strat1[i] == "R" && strat2[i] == "S") || (strat1[i] == "S" && strat2[i] == "P") || (strat1[i] == "P" && strat2[i] == "R"))
 		{
-			results[2*i] = 'W';
+			results.push_back('W');
 		}
 		//Check if player 2 beat player 1
 		else if ((strat1[i] == "R" && strat2[i] == "P") || (strat1[i] == "P" && strat2[i] == "S") || (strat1[i] == "S" && strat2[i] == "R"))
 		{
-			results[2*i] = 'L';
+			results.push_back('L');
+			//results[2*i] = 'L';
 		}
 		//Places a space between each letter
-		if(i > 0)
+		if(i != games-1)
 		{
-			results[2*i-1] = ' ';
+			results.push_back(' ');
+			//results[2*i-1] = ' ';
 		}
 	}
+
 }
 
 void referee::printResults()
 {
 	//Prints the wins vs losses to the console
-	if(char(results[0]) == "T")
-	{
-		cout << "1" << endl;
-	}
+	//if(char(results[0]) == "T")
+	//{
+	//	cout << "1" << endl;
+	//}
 	for (int i = 0; i < 2*games; i++)
 	{
-		cout << char(results[i]);
+		cout << results[i];
 	}
 	cout << endl;
 }
 
 referee::~referee()
 {
-	delete results;
+	//delete results;
 }
 
 
