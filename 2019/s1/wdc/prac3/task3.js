f45d9c49b41c18019a02aa234d9b0b8183984ee5
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-03-27 08:11:59 +0000

    plznotimeout

--- 2019/s1/wdc/prac3/task3.js
+++ 2019/s1/wdc/prac3/task3.js
@@ -1,12 +1,9 @@
 var inc = 0;
 var count = document.createElement("P");
 
-window.onload = function() {
+function myFunction() {
 	count.innerHTML = inc;
 	document.getElementById("mcount").appendChild(count);
-};
-
-function myFunction() {
 	inc++;
 	count.innerHTML = inc;
 }
