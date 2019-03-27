066980ae77efe78d8f57d465be51796e7fbfc035
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-03-27 08:18:39 +0000

    why

--- 2019/s1/wdc/prac3/task3.js
+++
@@ -1,9 +0,0 @@
-var inc = 0;
-var count = document.createElement("P");
-
-function myFunction() {
-	count.innerHTML = inc;
-	document.getElementById("mcount").appendChild(count);
-	inc++;
-	count.innerHTML = inc;
-}
