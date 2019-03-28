82b572d87abc5d946aec606596b8e91c04a7d6df
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-03-28 09:56:15 +0000

    task 3-2

--- 2019/s1/wdc/prac3/task3.js
+++ 2019/s1/wdc/prac3/task3.js
@@ -1,9 +1,22 @@
 var inc = 0;
 var count = document.createElement("P");
 
+var addDate = document.createElement("P");
+var addContent = document.createElement("P");
+addDate.className += "post-time";
+addContent.className += "post-content";
+
 function myFunction() {
 	count.innerHTML = inc;
 	document.getElementById("mcount").appendChild(count);
 	inc++;
 	count.innerHTML = inc;
+}
+
+function createPost() {
+	var date = new Date();
+	addDate.innerHTML = date;
+	addContent.innerHTML = document.getElementById("content-area").value; 
+	document.getElementById("posts").appendChild(addDate);
+	document.getElementById("posts").appendChild(addContent);
 }
