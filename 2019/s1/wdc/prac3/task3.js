d5358cbeb290a11188c5f342d9fc2acf5a9997dd
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-03-28 10:55:48 +0000

    task 3-5

--- 2019/s1/wdc/prac3/task3.js
+++ 2019/s1/wdc/prac3/task3.js
@@ -1,11 +1,6 @@
 var inc = 0;
 var count = document.createElement("P");
 
-var addDate = document.createElement("P");
-var addContent = document.createElement("P");
-addDate.className += "post-time";
-addContent.className += "post-content";
-
 function myFunction() {
 	count.innerHTML = inc;
 	document.getElementById("mcount").appendChild(count);
@@ -14,9 +9,35 @@ function myFunction() {
 }
 
 function createPost() {
-	var date = new Date();
-	addDate.innerHTML = date;
-	addContent.innerHTML = document.getElementById("content-area").value; 
-	document.getElementById("posts").appendChild(addDate);
-	document.getElementById("posts").appendChild(addContent);
+	for(var i = 0; i < document.getElementById("numButton").value; i++) {
+		var addDate = document.createElement("P");
+		var addContent = document.createElement("P");
+		addDate.className += "post-time";
+		addContent.className += "post-content";
+		var date = new Date();
+		addDate.innerHTML = date;
+		if(document.getElementById("redButton").checked) {
+			addContent.style.color = "Red";
+		}
+		if(document.getElementById("blueButton").checked) {
+			addContent.style.color = "Blue";
+		}
+		addContent.innerHTML = document.getElementById("content-area").value; 
+		document.getElementById("posts").appendChild(addDate);
+		document.getElementById("posts").appendChild(addContent);
+	}
+}
+
+function toggleMain() {
+	if (document.getElementById("main").style.display === "none") {
+    document.getElementById("main").style.display = "block";
+    document.getElementById("menu").style.display = "none";
+  } else {
+    document.getElementById("main").style.display = "none";
+    document.getElementById("menu").style.display = "block";
+  }
+}
+
+function changeBackground() {
+	document.body.style.backgroundColor = document.getElementById("backgroundBox").value;
 }
