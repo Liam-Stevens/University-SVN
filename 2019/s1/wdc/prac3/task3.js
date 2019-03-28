0c71ea26e34417cc0500f3c89d01ce8c00c8c7a8
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-03-28 11:08:26 +0000

    half task 3-6

--- 2019/s1/wdc/prac3/task3.js
+++ 2019/s1/wdc/prac3/task3.js
@@ -1,5 +1,6 @@
 var inc = 0;
 var count = document.createElement("P");
+var postNum = 0;
 
 function myFunction() {
 	count.innerHTML = inc;
@@ -10,19 +11,26 @@ function myFunction() {
 
 function createPost() {
 	for(var i = 0; i < document.getElementById("numButton").value; i++) {
+		postNum++;
 		var addDate = document.createElement("P");
 		var addContent = document.createElement("P");
 		addDate.className += "post-time";
 		addContent.className += "post-content";
 		var date = new Date();
 		addDate.innerHTML = date;
+		addContent.innerHTML = document.getElementById("content-area").value; 
 		if(document.getElementById("redButton").checked) {
 			addContent.style.color = "Red";
 		}
 		if(document.getElementById("blueButton").checked) {
 			addContent.style.color = "Blue";
 		}
-		addContent.innerHTML = document.getElementById("content-area").value; 
+		if(document.getElementById("boldBox").checked) {
+			addContent.innerHTML = addContent.innerHTML.bold();
+		}
+		if(document.getElementById("italBox").checked) {
+			addContent.innerHTML = addContent.innerHTML.italics();
+		}
 		document.getElementById("posts").appendChild(addDate);
 		document.getElementById("posts").appendChild(addContent);
 	}
@@ -41,3 +49,5 @@ function toggleMain() {
 function changeBackground() {
 	document.body.style.backgroundColor = document.getElementById("backgroundBox").value;
 }
+
+
