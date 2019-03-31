3461599c2d3e867bfb184c63fa11f3595db868a2
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-03-31 08:58:04 +0000

    stdout added

--- 2019/s1/spc/prac1/encode.sh
+++ 2019/s1/spc/prac1/encode.sh
@@ -2,7 +2,7 @@
 
 
 
-#Read input file
+#Read input file from stdin
 inputs=$#;
 if [ "$inputs" = "0" ]
 then
@@ -38,7 +38,24 @@ fi
 
 #Encoding
 fileOutput=$(echo $fileInput | $(./create_pattern.sh $1 $2 $3 $4))
-echo $fileOutput;
+echo $fileOutput > "${5:-/dev/stdout}";
+
+if [ "$inputs" = "0" ]
+then
+	echo $fileOutput > "${2:-/dev/stdout}";
+elif [ "$inputs" = "1" ]
+then
+	echo $fileOutput > "${3:-/dev/stdout}";
+elif [ "$inputs" = "2" ]
+then
+	echo $fileOutput > "${4:-/dev/stdout}";
+elif [ "$inputs" = "3" ]
+then
+	echo $fileOutput > "${5:-/dev/stdout}";
+elif [ "$inputs" = "4" ]
+then
+	echo $fileOutput > "${6:-/dev/stdout}";
+fi
 
 exit
 
