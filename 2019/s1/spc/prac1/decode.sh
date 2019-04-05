0de34a6c5552b911d72886d1521b1df6ede5133e
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-04-05 09:19:37 +0000

    p1fix

--- 2019/s1/spc/prac1/decode.sh
+++ 2019/s1/spc/prac1/decode.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+#Checks for an upper case encoding
 for (( i=1; i<27; i++))
 do
 	for (( j=i; j<27; j++ ))
 	do
+		#Makes a file with an encoding
 		let different=0;
 		$(./create_pattern.sh -u -s $i $j) < $1 > test.txt;
+		#Checks if the encoding is the same as the result
 		cmp -s test.txt $2 || let different=1;
 		rm test.txt;
+		#Outputs the encoding type if they are the same
 		if [ $different -eq 0 ]
 		then
 			echo $i $j "-u";
@@ -15,14 +19,18 @@ do
 	done
 done
 
+#Checks for lower case encoding
 for (( i=1; i<27; i++))
 do
 	for (( j=i; j<27; j++ ))
 	do
+		#Makes a file with an encoding
 		let different=0;
 		$(./create_pattern.sh -s $i $j) < $1 > test.txt;
+		#Checks if the encoding is the same as the result
 		cmp -s test.txt $2 || let different=1;
 		rm test.txt;
+		#Outputs the encoding type if they are the same
 		if [ $different -eq 0 ]
 		then
 			echo $i $j;
