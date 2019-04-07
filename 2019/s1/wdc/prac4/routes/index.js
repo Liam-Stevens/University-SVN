1beedc675c8e7e472bc8d41197aac8122fc4f1f3
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-04-07 04:52:14 +0000

    lint plz10

--- 2019/s1/wdc/prac4/routes/index.js
+++ 2019/s1/wdc/prac4/routes/index.js
@@ -29,13 +29,13 @@ router.get('/colour.html', function (req, res, next) {
 			<body> \
 			<h1 style="color:' + colorVis[colorTag] + '">' + colorVis[colorTag] + '</h1> \
 			</body> \
-			</html>');
+			</html>'); /*
   colorTag = colorTag + 1;
   
   if (colorTag > 3)
   {
   	colorTag = 0;
-  }
+  } */
 });
 
 router.get('/log.html', function (req, res, next) {
