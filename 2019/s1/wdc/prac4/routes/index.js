a4f3d775aa64e01dbe86630bbe74c665ee802ae0
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-04-07 03:58:30 +0000

    what

--- 2019/s1/wdc/prac4/routes/index.js
+++ 2019/s1/wdc/prac4/routes/index.js
@@ -16,57 +16,6 @@ router.get('/last.txt', function (req, res, next) {
   date = Date();
 });
 
-router.get('/colour.html', function (req, res, next) {
-  
-  res.send('<!DOCTYPE html> \
-			<html lang="en"> \
-			<head> \
-			<meta charset="UTF-8"> \
-			<title>  \
-			Colour Stuff \
-			</title> \
-			</head> \
-			<body> \
-			<h1 style="color:'+colorVis+'">'+colorVis+'</h1> \
-			</body> \
-			</html>');
-	
-  colorTag++;
-  if (colorTag > 3)
-  {
-  	colorTag = 0;
-  }
-  if (colorTag == 0)
-  {
-  	colorVis = "red";
-  }
-  else if (colorTag == 1)
-  {
-  	colorVis = "yellow";
-  }
-  else if (colorTag == 2)
-  {
-  	colorVis = "green";
-  }
-  else if (colorTag == 3)
-  {
-  	colorVis = "blue";
-  }
-});
 
-router.get('/log.html', function (req, res, next) {
-  var dateAdd = "<li>"+Date()+"</li>";
-  dateList = dateList.concat(dateAdd);
-  res.send('<!DOCTYPE html> \
-			<html lang="en"> \
-			<head> \
-			<meta charset="UTF-8"> \
-			<title>  \
-			Colour Stuff \
-			</title> \
-			</head> \
-			<body>'+dateList+'</body> \
-			</html>');
-});
 
 module.exports = router;
