c7ce32d23aa6d8d04a2cbdd1937e36fee5b33712
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-04-07 04:00:00 +0000

    plox

--- 2019/s1/wdc/prac4/routes/index.js
+++ 2019/s1/wdc/prac4/routes/index.js
@@ -16,6 +16,37 @@ router.get('/last.txt', function (req, res, next) {
   date = Date();
 });
 
+router.get('/colour.html', function (req, res, next) {
+  
+  res.send('<h1 style="color:'+colorVis+'">'+colorVis+'</h1>');
+	
+  colorTag++;
+  if (colorTag > 3)
+  {
+  	colorTag = 0;
+  }
+  if (colorTag == 0)
+  {
+  	colorVis = "red";
+  }
+  else if (colorTag == 1)
+  {
+  	colorVis = "yellow";
+  }
+  else if (colorTag == 2)
+  {
+  	colorVis = "green";
+  }
+  else if (colorTag == 3)
+  {
+  	colorVis = "blue";
+  }
+});
 
+router.get('/log.html', function (req, res, next) {
+  var dateAdd = "<li>"+Date()+"</li>";
+  dateList = dateList.concat(dateAdd);
+  res.send(dateList);
+});
 
 module.exports = router;
