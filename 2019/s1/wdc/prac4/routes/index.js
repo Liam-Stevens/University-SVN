3402b9126603819091afe6bea9369b94bc2e9d26
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-04-07 02:54:06 +0000

    P3.1

--- 2019/s1/wdc/prac4/routes/index.js
+++ 2019/s1/wdc/prac4/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var date = "";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/last.txt', function (req, res, next) {
+  res.send(date);
+  date = Date();
+});
+
 
 module.exports = router;
