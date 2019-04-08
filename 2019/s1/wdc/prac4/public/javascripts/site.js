e18f85176967e88d303770bb0b4228edc0e77afe
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-04-08 11:38:17 +0000

    P4.3

---
+++ 2019/s1/wdc/prac4/public/javascripts/site.js
@@ -0,0 +1,46 @@
+var xhttp = new XMLHttpRequest();
+
+function getContact() {
+
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200)
+		{
+			document.getElementById("addContent").innerHTML = this.responseText;
+		}
+	};
+
+	xhttp.open("GET","/contact.ajax",true);
+
+	xhttp.send();
+
+}
+
+function getSearch() {
+
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200)
+		{
+			document.getElementById("addContent").innerHTML = this.responseText;
+		}
+	};
+
+	xhttp.open("GET","/search.ajax",true);
+
+	xhttp.send();
+
+}
+
+function getAbout() {
+
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200)
+		{
+			document.getElementById("addContent").innerHTML = this.responseText;
+		}
+	};
+
+	xhttp.open("GET","/about.ajax",true);
+
+	xhttp.send();
+
+}
