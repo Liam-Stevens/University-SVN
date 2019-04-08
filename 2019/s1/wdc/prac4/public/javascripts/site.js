5c1eae5fd78d697857356e5e3c24028d02888dd5
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-04-08 11:51:46 +0000

    linted2

--- 2019/s1/wdc/prac4/public/javascripts/site.js
+++ 2019/s1/wdc/prac4/public/javascripts/site.js
@@ -3,44 +3,44 @@ var xhttp = new XMLHttpRequest();
 function getContact() {
 
 	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState == 4 && xhttp.status == 200)
+		if (xhttp1.readyState == 4 && xhttp1.status == 200)
 		{
 			document.getElementById("addContent").innerHTML = this.responseText;
 		}
 	};
 
-	xhttp.open("GET","/contact.ajax",true);
+	xhttp1.open("GET","/contact.ajax",true);
 
-	xhttp.send();
+	xhttp1.send();
 
 }
 
 function getSearch() {
 
 	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState == 4 && xhttp.status == 200)
+		if (xhttp2.readyState == 4 && xhttp2.status == 200)
 		{
 			document.getElementById("addContent").innerHTML = this.responseText;
 		}
 	};
 
-	xhttp.open("GET","/search.ajax",true);
+	xhttp2.open("GET","/search.ajax",true);
 
-	xhttp.send();
+	xhttp2.send();
 
 }
 
 function getAbout() {
 
 	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState == 4 && xhttp.status == 200)
+		if (xhttp3.readyState == 4 && xhttp3.status == 200)
 		{
 			document.getElementById("addContent").innerHTML = this.responseText;
 		}
 	};
 
-	xhttp.open("GET","/about.ajax",true);
+	xhttp3.open("GET","/about.ajax",true);
 
-	xhttp.send();
+	xhttp3.send();
 
 }
