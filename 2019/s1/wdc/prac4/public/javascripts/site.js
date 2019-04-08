4e0df1819bb5190a73fe98518f26f261e799b457
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-04-08 11:57:33 +0000

    test1

--- 2019/s1/wdc/prac4/public/javascripts/site.js
+++ 2019/s1/wdc/prac4/public/javascripts/site.js
@@ -1,46 +1,48 @@
+/*
 var xhttp = new XMLHttpRequest();
 
 function getContact() {
 
 	xhttp.onreadystatechange = function() {
-		if (xhttp1.readyState == 4 && xhttp1.status == 200)
+		if (xhttp.readyState == 4 && xhttp.status == 200)
 		{
 			document.getElementById("addContent").innerHTML = this.responseText;
 		}
 	};
 
-	xhttp1.open("GET","/contact.ajax",true);
+	xhttp.open("GET","/contact.ajax",true);
 
-	xhttp1.send();
+	xhttp.send();
 
 }
 
 function getSearch() {
 
 	xhttp.onreadystatechange = function() {
-		if (xhttp2.readyState == 4 && xhttp2.status == 200)
+		if (xhttp.readyState == 4 && xhttp.status == 200)
 		{
 			document.getElementById("addContent").innerHTML = this.responseText;
 		}
 	};
 
-	xhttp2.open("GET","/search.ajax",true);
+	xhttp.open("GET","/search.ajax",true);
 
-	xhttp2.send();
+	xhttp.send();
 
 }
 
 function getAbout() {
 
 	xhttp.onreadystatechange = function() {
-		if (xhttp3.readyState == 4 && xhttp3.status == 200)
+		if (xhttp.readyState == 4 && xhttp.status == 200)
 		{
 			document.getElementById("addContent").innerHTML = this.responseText;
 		}
 	};
 
-	xhttp3.open("GET","/about.ajax",true);
+	xhttp.open("GET","/about.ajax",true);
 
-	xhttp3.send();
+	xhttp.send();
 
 }
+*/
