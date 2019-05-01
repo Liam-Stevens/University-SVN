3caf0ad43947e59a84b92a718a6d5a2c93949081
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-05-01 11:32:52 +0000

    start

---
+++ 2019/s1/adds/assignment5/MapGeneric.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include "MapGeneric.h"
+
+using namespace std;
+
+vector<int> MapGeneric::map(vector<int> inputList)
+{
+	return inputList;
+}
---
+++ 2019/s1/adds/assignment5/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "MapGeneric.h"
+
+using namespace std;
+
+int main()
+{
+	/*
+	string line;
+	string input[20];
+	int inc = 0;
+
+	//Takes inputs
+	do {
+		cin >> line;
+		input[inc] = line;
+		inc++;
+	}
+	while(inc < 20);
+
+	cout << line << endl;
+	*/
+
+
+
+	return 0;
+}
