3182f64329de2aa257966c8422b0fe71facc2a91
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-05-05 07:41:27 +0000

    done2

--- 2019/s1/wdc/prac6/query3-5.sql
+++ 2019/s1/wdc/prac6/query3-5.sql
@@ -1,1 +1,1 @@
-SELECT name, mark FROM Students INNER JOIN Enrolments ON Students.student_id=Enrolments.student_id WHERE Enrolments.mark<50;
+SELECT name, mark FROM Students INNER JOIN Enrolments ON Students.student_id = Enrolments.student_id WHERE Enrolments.mark < 50 AND Enrolments.subject_code = "COMP SCI 2000";
