9c69dbfea4b2d5f2f13ed55a09766a2e48c1cf6a
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-05-05 07:29:55 +0000

    most q3

---
+++ 2019/s1/wdc/prac6/query3-2.sql
@@ -0,0 +1,1 @@
+SELECT subject FROM Subjects;
---
+++ 2019/s1/wdc/prac6/query3-5.sql
@@ -0,0 +1,1 @@
+SELECT name FROM Students INNER JOIN Enrolments ON Students.student_id=Enrolments.student_id WHERE Enrolments.subject_code="COMP SCI 2207";
