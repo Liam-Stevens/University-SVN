a17564e94b6c679bdf751d640de5a88c9f2064be
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-05-05 07:06:47 +0000

    q2

--- 2019/s1/wdc/prac6/task2.sql
+++ 2019/s1/wdc/prac6/task2.sql
@@ -22,6 +22,78 @@
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `enrolment` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `enrolment`;
+
+--
+-- Table structure for table `Enrolments`
+--
+
+DROP TABLE IF EXISTS `Enrolments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Enrolments` (
+  `student_id` varchar(8) DEFAULT NULL,
+  `subject_code` varchar(20) DEFAULT NULL,
+  `mark` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Enrolments`
+--
+
+LOCK TABLES `Enrolments` WRITE;
+/*!40000 ALTER TABLE `Enrolments` DISABLE KEYS */;
+INSERT INTO `Enrolments` VALUES ('a1111111','COMP SCI 1102',62),('a1111111','COMP SCI 2000',80),('a1111112','COMP SCI 1102',55),('a1111112','COMP SCI 2207',80),('a1111113','PHIL 2039',65),('a1111113','COMP SCI 1102',46),('a1111114','COMP SCI 2207',67),('a1111114','COMP SCI 2000',49);
+/*!40000 ALTER TABLE `Enrolments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Students`
+--
+
+DROP TABLE IF EXISTS `Students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Students` (
+  `student_id` varchar(8) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `program` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Students`
+--
+
+LOCK TABLES `Students` WRITE;
+/*!40000 ALTER TABLE `Students` DISABLE KEYS */;
+INSERT INTO `Students` VALUES ('a1111111','Fang Li','BE(Hons)(Soft)'),('a1111112','Jane Brown','BE(Hons)(Soft)'),('a1111113','Bob Smith','BCompSc'),('a1111114','Wei Zhang','BCompSc');
+/*!40000 ALTER TABLE `Students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Subjects`
+--
+
+DROP TABLE IF EXISTS `Subjects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Subjects` (
+  `subject_code` varchar(20) DEFAULT NULL,
+  `subject` varchar(40) DEFAULT NULL,
+  `faculty` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Subjects`
+--
+
+LOCK TABLES `Subjects` WRITE;
+/*!40000 ALTER TABLE `Subjects` DISABLE KEYS */;
+INSERT INTO `Subjects` VALUES ('COMP SCI 1102','Object Oriented Programming','ECMS'),('COMP SCI 2207','Web and Database Computing','ECMS'),('COMP SCI 2000','Computer Systems','ECMS'),('PHIL 2039','Philosophy of Mind','Arts');
+/*!40000 ALTER TABLE `Subjects` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -32,4 +104,4 @@ USE `enrolment`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-05 16:05:03
+-- Dump completed on 2019-05-05 16:36:41
