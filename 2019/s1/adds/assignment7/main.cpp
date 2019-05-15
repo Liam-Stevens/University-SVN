e6a5f5746cdff47825f4a7c5c7da60cea4520984
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-05-15 15:05:40 +0000

    finish

--- 2019/s1/adds/assignment7/main.cpp
+++ 2019/s1/adds/assignment7/main.cpp
@@ -2,6 +2,9 @@
 #include "Node.h"
 
 #include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -12,7 +15,7 @@ int main() {
 
 	test.setData(65);
 	cout << test.getData() << endl;
-	
+
 	Node * ptr;
 	ptr = &test2;
 	ptr->setData(88);
@@ -22,15 +25,73 @@ int main() {
 	cout << ptr2->getData() << endl;
 	*/
 
+	/*
 	int array[5] = {1,2,3,4,5};
 
 	LinkedList test(array,5);
+
+	test.deleteEnd();
+
+	test.addAtPosition(3, 8);
+
 	test.printItems();
 
-	test.deleteFront();
+	test.deletePosition(6);
 
 	test.printItems();
+	*/
 
-	test.search(3);
+
+	//Takes inputs
+    string line;
+	int numInput[100];
+	string funcInput[3];
+	int size = 0;
+	int	funcInc = 0;
+	bool end = false;
+
+	do {
+		cin >> line;
+
+		if(((int)line[0] - 48) < 10 && end == false)
+		{
+			numInput[size] = atoi (line.c_str());
+		} else {
+			end = true;
+		}
+
+		if(end == true)
+		{
+			funcInput[funcInc] = line;
+			funcInc++;
+		} else {
+			size++;
+		}
+	}
+	while(funcInc < 3);
+
+	//Create object
+	LinkedList list(numInput,size);
+
+	//Ifs
+	if(funcInput[0] == "AF") {
+		list.addFront(atoi (funcInput[1].c_str()));
+	} else if(funcInput[0] == "AE") {
+		list.addEnd(atoi (funcInput[1].c_str()));
+	} else if(funcInput[0] == "AP") {
+		list.addAtPosition(atoi (funcInput[1].c_str()),atoi (funcInput[2].c_str()));
+	} else if(funcInput[0] == "S") {
+		list.search(atoi (funcInput[1].c_str()));
+	} else if(funcInput[0] == "DF") {
+		list.deleteFront();
+	} else if(funcInput[0] == "DE") {
+		list.deleteEnd();
+	} else if(funcInput[0] == "DP") {
+		list.deletePosition(atoi (funcInput[1].c_str()));
+	} else if(funcInput[0] == "GI") {
+		list.getItem(atoi (funcInput[1].c_str()));
+	}
+
+	list.printItems();
 
 }
