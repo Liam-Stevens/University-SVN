768fcfe37975156868ba40f35559431d6bf70361
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-06-01 08:02:16 +0000

    exit fix

--- 2019/s1/spc/prac3/execute2.c
+++ 2019/s1/spc/prac3/execute2.c
@@ -17,80 +17,73 @@ int execute(char *argv[], int maxCom)
 	int	pid ;
 	int	child_info = -1;
 
-	if ( argv[0] == NULL )		/* nothing succeeds	*/
+	if ( argv[0] == NULL ) {		/* nothing succeeds	*/
 		return 0;
+	}
 
-	if ( (pid = fork())  == -1 )
-		perror("fork");
-	else if ( pid == 0 ){
+	//Testable pids for parent and children
+	int child = 0;
+	int i = 0;
+	int prev_pipe[2];
+	int new_pipe[2];
+	for(i; i < maxCom; i++) {
+		//Create a new pipe
+		if (i != maxCom - 1) //Not the final command
+		{
+			pipe(new_pipe);
+		}
 
-		//Testable pids for parent and children
-	    int child = 0;
-	    int i = 0;
-	    int prev_pipe[2];
-	    int new_pipe[2];
-	    for(i; i < maxCom; i++) {
-	        //Create a new pipe
-	        if (i != maxCom - 1) //Not the final command
-	        {
-	            pipe(new_pipe);
-	        }
+		//Forks and checks for errors
+		child = fork();
+		if(child < 0) {
+			printf("Error");
+		}
 
-	        //Forks and checks for errors
-	        child = fork();
-	        if(child < 0) {
-	            printf("Error");
-	        }
-
-	        if(child != 0){
-	            //Parent
-	            wait(NULL);
-	            if(i != 0) //not first command
-	            {
-	                //close prev_pipe
-	                close(prev_pipe[0]);
-	                //close(prev_pipe[1]);
-	            }
-	            if(i != maxCom - 1) //not final command
-	            {
-	                //prev_pipe = new_pipe
-	                prev_pipe[0] = new_pipe[0];
-	                prev_pipe[1] = new_pipe[1];
-	                close(prev_pipe[1]);
-	            }
-	        }
-	        else
-	        {
-	            //Child
-	            if(i != 0) //not first command
-	            {
-	                //redirect input to prev_pipe
-	                dup2 (prev_pipe[0], 0);
-	                close(prev_pipe[0]);
-	            }
-	            if(i != maxCom - 1) //not final command
-	            {
-	                //redirect output to new_pipe
-	                dup2(new_pipe[1], 1);
-	                close(new_pipe[1]);
-	            }
-
-
-				signal(SIGINT, SIG_DFL);
-				signal(SIGQUIT, SIG_DFL);
-				execvp(argv[2*i], argv);
-				perror("cannot execute command");
-				exit(1);
-
+		if(child != 0){
+			//Parent
+			wait(NULL);
+			if(i != 0) //not first command
+			{
+				//close prev_pipe
+				close(prev_pipe[0]);
+				//close(prev_pipe[1]);
+			}
+			if(i != maxCom - 1) //not final command
+			{
+				//prev_pipe = new_pipe
+				prev_pipe[0] = new_pipe[0];
+				prev_pipe[1] = new_pipe[1];
+				close(prev_pipe[1]);
+			}
+		}
+		else
+		{
+			//Child
+			if(i != 0) //not first command
+			{
+				//redirect input to prev_pipe
+				dup2 (prev_pipe[0], 0);
+				close(prev_pipe[0]);
+			}
+			if(i != maxCom - 1) //not final command
+			{
+				//redirect output to new_pipe
+				dup2(new_pipe[1], 1);
+				close(new_pipe[1]);
 			}
 
 
+			signal(SIGINT, SIG_DFL);
+			signal(SIGQUIT, SIG_DFL);
+			execvp(argv[2*i], argv);
+			perror("cannot execute command");
+			exit(1);
 
 		}
+
+
+
 	}
-	else {
-		if ( wait(&child_info) == -1 )
-			perror("wait");
-	}
+
 	return child_info;
 }
