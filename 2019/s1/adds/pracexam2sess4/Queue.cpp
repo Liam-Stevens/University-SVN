667c52095a7019e8d6f76f1ca3cea9567c92a225
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-06-04 02:35:46 +0000

    q2maybe

--- 2019/s1/adds/pracexam2sess4/Queue.cpp
+++ 2019/s1/adds/pracexam2sess4/Queue.cpp
@@ -1,2 +1,32 @@
 #include "Queue.h"
 
+Queue::Queue() {
+	start = 0;
+	end = 0;
+	count = 0;
+}
+
+void Queue::add(int val) {
+	if(count < 8) {
+		content[end] = val;
+		count++;
+		end = count;
+	}
+	
+}
+
+int Queue::remove() {
+	if(count == 0)
+	{
+		return -1;
+	}
+	else
+	{
+		for (int i = 0; i < count-1; i++)
+		{
+			content[i] = content[i+1];
+		}
+		count--;
+		end = count;
+	}
+}
