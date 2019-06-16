280ae44cf3fbb140b6b0ff4794b2c7c25672bb9f
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-06-16 07:42:12 +0000

    1-4

---
+++ 2019/s1/wdc/prac9/query1-4.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT address FROM address INNER JOIN store ON(store.address_id=address.address_id) INNER JOIN inventory ON(inventory.store_id=store.store_id) INNER JOIN film ON(film.film_id=inventory.film_id) WHERE film.title="TWISTED PIRATES";
