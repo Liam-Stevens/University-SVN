593e90b5dc198dfcaead19ce0f7eaece92067a65
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-08-01 03:31:26 +0000

    Inchworm2

--- 2019/s2/pssd/week01practice/Inchworm.hpp
+++ 2019/s2/pssd/week01practice/Inchworm.hpp
@@ -5,7 +5,38 @@ class Inchworm
   public:
   int lunchtime(int branch, int rest, int leaf)
   {
-    int ans = (branch/rest)/leaf;
-    return ans;
+  	int eaten = 1;
+  	bool done = false;
+  	int leafPos = leaf;
+  	int wormPos = 0;
+  	while (done == false)
+  	{
+  		wormPos = wormPos + rest;
+  		if (wormPos > branch)
+  		{
+  			done = true;
+  			break;
+  		}
+  		else
+  		{
+  			if (wormPos == leafPos)
+  			{
+  				eaten++;
+  				leafPos = leafPos + leaf;
+  			} else if (wormPos > leafPos)
+  			{
+  				while(leafPos < wormPos)
+  				{
+  					leafPos = leafPos + leaf;
+  				}
+  			}
+  			else
+  			{
+
+  			}
+  		}
+  	}
+
+    return eaten;
   }
 };
