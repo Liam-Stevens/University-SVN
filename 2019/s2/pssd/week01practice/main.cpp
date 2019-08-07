eb19dce5f1f1e3f02b5112cdd9e72cd785585ad3
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-08-07 08:19:49 +0000

    DownloadFiles 01

--- 2019/s2/pssd/week01practice/main.cpp
+++ 2019/s2/pssd/week01practice/main.cpp
@@ -1,11 +1,18 @@
-#include "ThrowTheBall.hpp"
+#include "DownloadingFiles.hpp"
 #include <iostream>
+#include <string>
+#include <ctype.h>
 
 using namespace std;
 
 int main()
 {
-	ThrowTheBall test;
-	cout << test.timesThrown(5,3,2) << endl;
+	vector<string> test32;
+	test32.push_back("1 10");
+	test32.push_back("1 20");
+	test32.push_back("2 40");
+	DownloadingFiles test;
+	cout << test.actualTime(test32) << endl;
+
 	return 0;
 }
