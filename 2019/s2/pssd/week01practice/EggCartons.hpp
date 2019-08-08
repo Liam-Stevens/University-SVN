e1bab3e6557991c4321e904f8218027ad4761194
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-08-08 07:48:41 +0000

    Week01 Q05 T03

--- 2019/s2/pssd/week01practice/EggCartons.hpp
+++ 2019/s2/pssd/week01practice/EggCartons.hpp
@@ -13,16 +13,32 @@ class EggCartons
         } else if(eggs < 6) {
             min = -1;
         } else {
-            cout << "Eggs: " << eggs << " Min: " << min << endl;
+
             min = min + 3*(eggs/24);
             eggs = eggs % 24;
-            min = min + (eggs/6);
-            eggs = eggs % 6;
-            cout << "Eggs: " << eggs << " Min: " << min << endl;
 
-            if(eggs < 6 && eggs != 0) {
-                min = -1;
+            for (int i = 0; i < n; i++)
+            {
+                if(eggs % 8 == 0) {
+                    min = min + (eggs/8);
+                    eggs = eggs % 8;
+                    break;
+                }
+                if(eggs % 6 == 0) {
+                    min = min + (eggs/6);
+                    eggs = eggs % 6;
+                    break;
+                }
+
+                eggs = eggs - 8;
+                min++;
+
+                if(eggs < 6 && eggs != 0) {
+                    min = -1;
+                    break;
+                }
             }
+
         }
 
 
