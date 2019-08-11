242f4025785345a2f649c9de96e9998ef4d56507
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-08-11 09:38:31 +0000

    Week02 Q05 T11

--- 2019/s2/pssd/week02practice/AlternateColors.hpp
+++ 2019/s2/pssd/week02practice/AlternateColors.hpp
@@ -64,16 +64,133 @@ class AlternateColors
         long inc = 0;
         long runTime = (r + g + b);
         int none = 0;
+        long min = r;
+        int minPoint = 0;
+        bool zero[3];
+        int zeroes = 0;
+        int index = k;
 
-        for (long i = 0; i < runTime; i++)
+        for (int i = 0; i < 3; i++)
         {
-            //cout << i << endl;
-            //cout << none << endl;
-            if (none >= 10000)
+            zero[i] = false;
+        }
+
+
+        //Reduce to series
+        for (int i = 0; i < 3; i++)
+        {
+            if(r != 0)
             {
-                break;
+                min = r;
+            } else if (g != 0) {
+                min = g;
+            } else if (b != 0) {
+                min = b;
             }
 
+            if (r == 0 && zero[0] == false)
+            {
+                zero[0] = true;
+                zeroes++;
+            }
+            if (g == 0 && zero[1] == false)
+            {
+                zero[1] = true;
+                zeroes++;
+            }
+            if (b == 0 && zero[2] == false)
+            {
+                zero[2] = true;
+                zeroes++;
+            }
+
+            if(r < min && zero[0] == false)
+            {
+                min = r;
+                minPoint = 0;
+            }
+            if(g < min && zero[1] == false)
+            {
+                min = g;
+                minPoint = 1;
+            }
+            if(b < min && zero[2] == false)
+            {
+                min = b;
+                minPoint = 2;
+            }
+
+            //cout << "R " << r << " G " << g << " B " << b << " Min " << min << " Zeroes " << zeroes << endl;
+            if ((min*(3-zeroes)) <= k && zeroes < 2)
+            {
+                if(zero[0] == false)
+                {
+                    r = r-min;
+                }
+                if(zero[1] == false)
+                {
+                    g = g-min;
+                }
+                if(zero[2] == false)
+                {
+                    b = b-min;
+                }
+                k = k-(min*(3-zeroes));
+
+                if(k == 0) {
+                    if (minPoint == 0)
+                    {
+                        color = "RED";
+                        break;
+                    }
+                    else if (minPoint == 1)
+                    {
+                        color = "GREEN";
+                        break;
+                    }
+                    else if (minPoint == 2)
+                    {
+                        color = "BLUE";
+                        break;
+                    }
+                }
+            }
+        }
+
+        int remain = k%(3-zeroes);
+        //cout << remain << zeroes << endl;
+        if(remain == 0 && zeroes == 0) {
+            color = "BLUE";
+        } else if (remain == 1 && zeroes == 0) {
+            color = "RED";
+        } else if (remain == 2 && zeroes == 0) {
+            color = "GREEN";
+        } else if (remain == 0 && zeroes == 1 && zero[0] == true) {
+            color = "BLUE";
+        } else if (remain == 0 && zeroes == 1 && zero[1] == true) {
+            color = "BLUE";
+        } else if (remain == 0 && zeroes == 1 && zero[2] == true) {
+            color = "GREEN";
+        } else if (remain == 1 && zeroes == 1 && zero[0] == true) {
+            color = "GREEN";
+        } else if (remain == 1 && zeroes == 1 && zero[1] == true) {
+            color = "RED";
+        } else if (remain == 1 && zeroes == 1 && zero[2] == true) {
+            color = "RED";
+        } else if (zeroes == 2 && zero[0] == false) {
+            color = "RED";
+        } else if (zeroes == 2 && zero[1] == false) {
+            color = "GREEN";
+        } else if (zeroes == 2 && zero[2] == false) {
+            color = "BLUE";
+        }
+
+
+        /*
+        for (long i = 0; i < runTime; i++)
+        {
+
+            //Find the lowest common multiple
             if(r > 10000 && g > 10000 && b > 10000 && k > 10000 && none < 10000) {
                 //All Reduction
                 long multiple = findMultiple(r,g,b,k);
@@ -86,7 +203,6 @@ class AlternateColors
                 b = b/multiple;
                 k = k/multiple;
                 //cout << "Executed at: R " << r << " G " << g << " B " << b << " K " << k << endl;
-
             } else if (r > 10000 && g > 10000  && k > 10000 && none < 10000) {
                 //Red and Green Reduction
                 long multiple = findMultiple(r,g,k);
@@ -98,7 +214,6 @@ class AlternateColors
                 g = g/multiple;
                 k = k/multiple;
                 //cout << "Executed at: R " << r << " G " << g << " K " << k << endl;
-
             } else if (r > 10000 && b > 10000 && k > 10000 && none < 10000) {
                 //Red and Blue Reduction
                 long multiple = findMultiple(r,b,k);
@@ -110,7 +225,6 @@ class AlternateColors
                 b = b/multiple;
                 k = k/multiple;
                 //cout << "Executed at: R " << r << " B " << b << " K " << k << endl;
-
             } else if (g > 10000 && b > 10000 && k > 10000 && none < 10000) {
                 //Green and Blue Reduction
                 long multiple = findMultiple(g,b,k);
@@ -122,7 +236,6 @@ class AlternateColors
                 b = b/multiple;
                 k = k/multiple;
                 //cout << "Executed at: G " << g << " B " << b << " K " << k << endl;
-
             }
 
             if(r > 0)
@@ -172,7 +285,7 @@ class AlternateColors
                 break;
             }
 
-        }
+        }*/
 
         return color;
     }
