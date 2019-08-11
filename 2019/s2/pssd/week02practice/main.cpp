d8ade9c9f3e7e9916721e20d6da71b1de1532e12
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-08-11 05:47:54 +0000

    Week02 Q05 T02

--- 2019/s2/pssd/week02practice/main.cpp
+++ 2019/s2/pssd/week02practice/main.cpp
@@ -13,10 +13,10 @@ int main()
 	//test32.push_back("....C");
 	//test32.push_back("CC...");
 	AlternateColors test;
-	long r = 1;
+	long r = 1000000000000;
 	long g = 1;
 	long b = 1;
-	long k = 3;
+	long k = 1000000000002;
 	cout << test.getColor(r, g, b, k) << endl;
 
 	return 0;
