5d6a7a4880db591dc6f84757fc997c21aefc6706
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-08-12 05:18:14 +0000

    Week02 Q02 T06

--- 2019/s2/pssd/week02practice/AlternateColors.hpp
+++ 2019/s2/pssd/week02practice/AlternateColors.hpp
@@ -4,62 +4,10 @@ using namespace std;
 
 class AlternateColors
 {
-    private:
-    bool check(long goal) {
-        if(goal == 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    long findMultiple(long in1,long in2, long in3, long in4)
-    {
-        long ans = 0;
-        bool found = false;
-        for(int i = 0; i < 10000; i++)
-        {
-                ans++;
-                if(ans % in1 == 0 && ans % in2 == 0 && ans % in3 == 0 && ans % in4 == 0)
-                {
-                    //cout << "!" << ans << endl;
-                    found = true;
-                    break;
-                }
-                //cout << ans << endl;
-        }
-        if (found == true)
-        {
-            return ans;
-        }  else {
-            return 1;
-        }
-    }
-    long findMultiple(long in1,long in2, long in3)
-    {
-        long ans = 0;
-        bool found = false;
-        for(int i = 0; i < 10000; i++)
-        {
-                ans++;
-                if(ans % in1 == 0 && ans % in2 == 0 && ans % in3 == 0)
-                {
-                    //cout << "!" << ans << endl;
-                    found = true;
-                    break;
-                }
-                //cout << ans << endl;
-        }
-        if (found == true)
-        {
-            return ans;
-        }  else {
-            return 1;
-        }
-    }
-
     public:
     string getColor(long r, long g, long b, long k)
     {
+        //Initialize
         string color = "NONE";
         long inc = 0;
         long runTime = (r + g + b);
@@ -79,6 +27,7 @@ class AlternateColors
         //Reduce to series
         for (int i = 0; i < 3; i++)
         {
+            //Ensure a non-zero minimum exists
             if(r != 0)
             {
                 min = r;
@@ -88,6 +37,7 @@ class AlternateColors
                 min = b;
             }
 
+            //Determine which has nothing left
             if (r == 0 && zero[0] == false)
             {
                 zero[0] = true;
@@ -104,6 +54,7 @@ class AlternateColors
                 zeroes++;
             }
 
+            //Find the minimum
             if(r < min && zero[0] == false)
             {
                 min = r;
@@ -120,9 +71,10 @@ class AlternateColors
                 minPoint = 2;
             }
 
-            //cout << "R " << r << " G " << g << " B " << b << " Min " << min << " Zeroes " << zeroes << endl;
+            //Reduce the scope of the problem
             if ((min*(3-zeroes)) <= k && zeroes < 2)
             {
+                //Reduce everything by a factor of minimum
                 if(zero[0] == false)
                 {
                     r = r-min;
@@ -137,6 +89,7 @@ class AlternateColors
                 }
                 k = k-(min*(3-zeroes));
 
+                //If k is reduced to nothing, find the result
                 if(k == 0) {
                     if (minPoint == 0)
                     {
@@ -157,8 +110,10 @@ class AlternateColors
             }
         }
 
+        //Calculate the remainer
         int remain = k%(3-zeroes);
-        //cout << remain << zeroes << endl;
+
+        //Large If block to determine the colour
         if(remain == 0 && zeroes == 0) {
             color = "BLUE";
         } else if (remain == 1 && zeroes == 0) {
@@ -184,108 +139,6 @@ class AlternateColors
         } else if (zeroes == 2 && zero[2] == false) {
             color = "BLUE";
         }
-
-
-        /*
-        for (long i = 0; i < runTime; i++)
-        {
-
-            //Find the lowest common multiple
-            if(r > 10000 && g > 10000 && b > 10000 && k > 10000 && none < 10000) {
-                //All Reduction
-                long multiple = findMultiple(r,g,b,k);
-                if (multiple == 1)
-                {
-                    none++;
-                }
-                r = r/multiple;
-                g = g/multiple;
-                b = b/multiple;
-                k = k/multiple;
-                //cout << "Executed at: R " << r << " G " << g << " B " << b << " K " << k << endl;
-            } else if (r > 10000 && g > 10000  && k > 10000 && none < 10000) {
-                //Red and Green Reduction
-                long multiple = findMultiple(r,g,k);
-                if (multiple == 1)
-                {
-                    none++;
-                }
-                r = r/multiple;
-                g = g/multiple;
-                k = k/multiple;
-                //cout << "Executed at: R " << r << " G " << g << " K " << k << endl;
-            } else if (r > 10000 && b > 10000 && k > 10000 && none < 10000) {
-                //Red and Blue Reduction
-                long multiple = findMultiple(r,b,k);
-                if (multiple == 1)
-                {
-                    none++;
-                }
-                r = r/multiple;
-                b = b/multiple;
-                k = k/multiple;
-                //cout << "Executed at: R " << r << " B " << b << " K " << k << endl;
-            } else if (g > 10000 && b > 10000 && k > 10000 && none < 10000) {
-                //Green and Blue Reduction
-                long multiple = findMultiple(g,b,k);
-                if (multiple == 1)
-                {
-                    none++;
-                }
-                g = g/multiple;
-                b = b/multiple;
-                k = k/multiple;
-                //cout << "Executed at: G " << g << " B " << b << " K " << k << endl;
-            }
-
-            if(r > 0)
-            {
-                r--;
-                k--;
-                if (check(k))
-                {
-                    color = "RED";
-                    break;
-                }
-            }
-            if(g > 0)
-            {
-                g--;
-                k--;
-                if (check(k))
-                {
-                    color = "GREEN";
-                    break;
-                }
-            }
-            if(b > 0)
-            {
-                b--;
-                k--;
-                if (check(k))
-                {
-                    color = "BLUE";
-                    break;
-                }
-            }
-
-            if (r == 0 && g == 0)
-            {
-                color = "BLUE";
-                break;
-            }
-            if (g == 0 && b == 0)
-            {
-                color = "RED";
-                break;
-            }
-            if (b == 0 && r == 0)
-            {
-                color = "GREEN";
-                break;
-            }
-
-        }*/
 
         return color;
     }
