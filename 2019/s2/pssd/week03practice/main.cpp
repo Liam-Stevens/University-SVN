489c9d8aadb394d74723195c531743e92dc7d773
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-08-14 07:18:36 +0000

    Week03 Q01 T01

---
+++ 2019/s2/pssd/week03practice/main.cpp
@@ -0,0 +1,19 @@
+#include "Arrows.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctype.h>
+
+using namespace std;
+
+int main()
+{
+	//vector<string> test32;
+	//test32.push_back("C....");
+	//test32.push_back("....C");
+	//test32.push_back("CC...");
+	Arrows test;
+	cout << test.longestArrow("<----=====>") << endl;
+
+	return 0;
+}
