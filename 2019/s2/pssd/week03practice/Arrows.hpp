f93afd3256b2aa741d31ab86dbb65aa0f06acb78
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-08-14 07:18:36 +0000

    Week03 Q01 T01

---
+++ 2019/s2/pssd/week03practice/Arrows.hpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Arrows
+{
+private:
+    int max;
+    int consecArrowNum;
+    bool left;
+    bool doubleArrow;
+
+    void reset()
+    {
+        consecArrowNum = 0;
+        left = false;
+        doubleArrow = false;
+    }
+
+    void update()
+    {
+        //cout << "PRE Max: " << max << " ArrowNum: " << consecArrowNum << endl;
+        if (max < consecArrowNum)
+        {
+            max = consecArrowNum;
+        }
+        //cout << "Max: " << max << " ArrowNum: " << consecArrowNum << endl;
+    }
+
+public:
+    int longestArrow(string s)
+    {
+        max = 0;
+        reset();
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == '>')
+            {
+                if (left != true)
+                {
+                    consecArrowNum++;
+                }
+                //cout << "Pass: " << i << " RIGHT" << endl;
+                update();
+                reset();
+            }
+
+            if(s[i] == '-' && doubleArrow == false)
+            {
+                consecArrowNum++;
+                //cout << "Pass: " << i << " B" << " Arrow Num: " << consecArrowNum << endl;
+            } else if (s[i] == '=' && doubleArrow == false)
+            {
+                if (left == true)
+                {
+                    update();
+                    //cout << "Pass: " << i << " C" << endl;
+                }
+                reset();
+                consecArrowNum++;
+                doubleArrow = true;
+                //cout << "Pass: " << i << " D" << endl;
+            } else if (s[i] == '-' && doubleArrow == true)
+            {
+                if (left == true)
+                {
+                    update();
+                    //cout << "Pass: " << i << " E" << endl;
+                }
+                reset();
+                consecArrowNum++;
+                //cout << "Pass: " << i << " F" << endl;
+            } else if (s[i] == '=' && doubleArrow == true)
+            {
+                consecArrowNum++;
+                //cout << "Pass: " << i << " G" << endl;
+            } else if (s[i] != '-' && doubleArrow == false && consecArrowNum < 0)
+            {
+                update();
+                reset();
+                //cout << "Pass: " << i << " H" << endl;
+            } else {
+                //cout << "Pass: " << i << " SKIP" << endl;
+            }
+
+            if(s[i] == '<')
+            {
+                update();
+                reset();
+                left = true;
+                consecArrowNum++;
+                //cout << "Pass: " << i << " LEFT" << endl;
+            }
+
+
+
+        }
+
+        if(max == 0)
+        {
+            max = -1;
+        }
+
+
+        return max;
+    }
+};
