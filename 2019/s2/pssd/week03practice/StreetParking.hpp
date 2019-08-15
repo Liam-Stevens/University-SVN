da9d1f75df144944e19a3034b4bcda9f36d572e7
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-08-15 04:13:23 +0000

    End of Prac

---
+++ 2019/s2/pssd/week03practice/StreetParking.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class StreetParking
+{
+
+public:
+    int freeParks(string street)
+    {
+        int parks = 0;
+
+        for (int i = 0; i < street.length(); i++)
+        {
+            
+        }
+
+
+        return parks;
+    }
+};
