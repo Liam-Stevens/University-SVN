9380ece0f7a9becce8b9bd6032d0da8ece5791b0
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-08-15 03:29:52 +0000

    Week03 Q02 T01

--- 2019/s2/pssd/week03practice/main.cpp
+++ 2019/s2/pssd/week03practice/main.cpp
@@ -1,4 +1,4 @@
-#include "Arrows.hpp"
+#include "SquareOfDigits.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,12 +8,24 @@ using namespace std;
 
 int main()
 {
-	//vector<string> test32;
-	//test32.push_back("C....");
-	//test32.push_back("....C");
-	//test32.push_back("CC...");
-	Arrows test;
-	cout << test.longestArrow("<===========") << endl;
+	vector<string> test32;
+	string test24[11] = {"9785409507",
+ "2055103694",
+ "0861396761",
+ "3073207669",
+ "1233049493",
+ "2300248968",
+ "9769239548",
+ "7984130001",
+ "1670020095",
+ "8894239889",
+ "4053971072"};
+ test32.assign(test24,test24+11);
+	//test32.push_back("42101");
+	//test32.push_back("22100");
+	//test32.push_back("22101");
+	SquareOfDigits test;
+	cout << test.getMax(test32) << endl;
 
 	return 0;
 }
