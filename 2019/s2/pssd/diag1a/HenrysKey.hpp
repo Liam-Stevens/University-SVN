4c224c4dd728f3d1d79f887e975896e0ba3e45ab
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-08-16 03:27:43 +0000

    ws1 diagram

---
+++ 2019/s2/pssd/diag1a/HenrysKey.hpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class HenrysKey
+{
+    public:
+    int howMany(vector<string> S)
+    {
+        int elements = S.size();
+        int found[elements];
+
+        for (int i = 0; i < elements; i++)
+        {
+            found[i] = 0;
+        }
+
+        for (int i = 0; i < elements; i++)
+        {
+            
+        }
+
+        
+
+        return numOfDiff;
+    }
+};
