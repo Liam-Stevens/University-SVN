1c45ebe6612dd091ea9fabac1f3309cc6b149cdd
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-08-17 07:52:37 +0000

    Week03 Q05 T01

--- 2019/s2/pssd/week03practice/main.cpp
+++ 2019/s2/pssd/week03practice/main.cpp
@@ -1,4 +1,4 @@
-#include "ExerciseMachine.hpp"
+#include "ElevatorLimit.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,12 +8,32 @@ using namespace std;
 
 int main()
 {
-	//vector<string> test32;
-	//test32.push_back("42101");
-	//test32.push_back("22100");
-	//test32.push_back("22101");
-	ExerciseMachine test;
-	cout << test.getPercentages("19:21:52") << endl;
+	vector<int> test32;
+	test32.push_back(6);
+	test32.push_back(85);
+	test32.push_back(106);
+	test32.push_back(1);
+	test32.push_back(199);
+	test32.push_back(76);
+	test32.push_back(162);
+	test32.push_back(141);
+	vector<int> test91;
+	test91.push_back(38);
+	test91.push_back(68);
+	test91.push_back(62);
+	test91.push_back(83);
+	test91.push_back(170);
+	test91.push_back(12);
+	test91.push_back(61);
+	test91.push_back(114);
+	ElevatorLimit test;
+	vector<int> results = test.getRange(test32,test91,668);
+
+	for (int i = 0; i < results.size(); i++)
+	{
+		cout << results[i] << " | ";
+	}
+	cout << endl;
 
 	return 0;
 }
