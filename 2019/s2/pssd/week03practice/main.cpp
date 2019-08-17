0c62d17ace38ce8ecec72dccb31282c77f0b7a84
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-08-17 08:01:22 +0000

    Week03 Q05 T02

--- 2019/s2/pssd/week03practice/main.cpp
+++ 2019/s2/pssd/week03practice/main.cpp
@@ -9,25 +9,13 @@ using namespace std;
 int main()
 {
 	vector<int> test32;
-	test32.push_back(6);
-	test32.push_back(85);
-	test32.push_back(106);
-	test32.push_back(1);
-	test32.push_back(199);
-	test32.push_back(76);
-	test32.push_back(162);
-	test32.push_back(141);
+	test32.push_back(0);
+	test32.push_back(2);
 	vector<int> test91;
-	test91.push_back(38);
-	test91.push_back(68);
-	test91.push_back(62);
-	test91.push_back(83);
-	test91.push_back(170);
-	test91.push_back(12);
-	test91.push_back(61);
-	test91.push_back(114);
+	test91.push_back(1);
+	test91.push_back(0);
 	ElevatorLimit test;
-	vector<int> results = test.getRange(test32,test91,668);
+	vector<int> results = test.getRange(test32,test91,1);
 
 	for (int i = 0; i < results.size(); i++)
 	{
