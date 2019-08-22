edbd660f86ff68515005b1c144cd513ed54fcec9
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-08-22 03:58:19 +0000

    Prac Exam 1 A1 T03

--- 2019/s2/pssd/pracexam1a1/main.cpp
+++ 2019/s2/pssd/pracexam1a1/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	ChickensAndCows test;
-	vector<int> tester = test.howMany(3,8);
+	vector<int> tester = test.howMany(359910,769710);
 	for (int i = 0; i < 2; i++)
 	{
 		cout << tester[i] << endl;
