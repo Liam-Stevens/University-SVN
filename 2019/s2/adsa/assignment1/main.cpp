27a338df6cef19a3bc1b53ab2c67b6bfb38118c6
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-08-25 07:07:29 +0000

    karatsuba retry

--- 2019/s2/adsa/assignment1/main.cpp
+++ 2019/s2/adsa/assignment1/main.cpp
@@ -5,6 +5,14 @@
 
 using namespace std;
 
+void printNumber(vector<int> num)
+{
+	for (int i = 0; i < num.size(); i++)
+	{
+		cout << num[i];
+	}
+}
+
 int indexer (int bottom, int top)
 {
 	if (bottom == 2 && top == 2)
@@ -57,19 +65,39 @@ int uniqueIndexer (int bottom, int top)
 	}
 }
 
+vector<int> removeLeading(vector<int> num)
+{
+	//Removes leading zeroes
+	int cut = 0;
+	for (int i = 0; i < num.size()-1; i++)
+	{
+		if (num[i] == 0)
+		{
+			cut++;
+		} else {
+			break;
+		}
+	}
+	if (cut != 0)
+	{
+		num.erase(num.begin(),num.begin()+cut);
+	}
+	return num;
+}
+
 vector<int> separate(vector<int> num, int location, bool section)
 {
 	if (section == false)
 	{
 		vector<int> separator(location + num.size()%2,0);
-		for (int i = 0; i < location; i++) {
+		for (int i = 0; i < location + num.size()%2; i++) {
 			separator[i] = num[i];
 		}
 		return separator;
 	} else {
 		vector<int> separator(location,0);
-		for (int i = location; i < num.size(); i++) {
-			separator[i - location] = num[i];
+		for (int i = location + num.size()%2; i < num.size(); i++) {
+			separator[i - (location + num.size()%2)] = num[i];
 		}
 		return separator;
 	}
@@ -83,11 +111,13 @@ vector<int> schoolAddition(vector<int> first, vector<int> second, int base)
 	{
 		maxLength = second.size();
 	}
+	//Extra room for the addition
 	maxLength++;
 	vector<int> workingFirstNumber = first;
 	workingFirstNumber.insert(workingFirstNumber.begin(),0);
 	vector<int>	workingSecondNumber = second;
 	workingSecondNumber.insert(workingSecondNumber.begin(),0);
+	//Pads zeroes until both sides are equal
 	while(workingFirstNumber.size() < workingSecondNumber.size())
 	{
 		workingFirstNumber.insert(workingFirstNumber.begin(),0);
@@ -116,21 +146,6 @@ vector<int> schoolAddition(vector<int> first, vector<int> second, int base)
 			sum[i] = sum[i] + base;
 			carry--;
 		}
-	}
-
-	int cut = 0;
-	for (int i = 0; i < sum.size()-1; i++)
-	{
-		if (sum[i] == 0)
-		{
-			cut++;
-		} else {
-			break;
-		}
-	}
-	if (cut != 0)
-	{
-		sum.erase(sum.begin(),sum.begin()+cut);
 	}
 
 	return sum;
@@ -191,27 +206,128 @@ vector<int> negative (vector<int> num)
 //The recursive function of karatsuba multiplication
 vector<int> recursiveKaratsuba (vector<int> first, vector<int> second, int base)
 {
+	/*cout << first.size() << " | " << second.size() << " {} " << endl;
+	cout << "Number1: ";
+	printNumber(first);
+	cout << " Number2: ";
+	printNumber(second);
+	cout << endl;*/
 	//Base Case
 	if (first.size() <= 3 && second.size() <= 3)
-	{
+	{/*
+		cout << "Multiplier: ";
+		printNumber(schoolMultiplication(first,second,base));
+		cout << endl;*/
 		return schoolMultiplication(first,second,base);
 	}
+	//cout << first.size() << " | " << second.size() << endl;
 
 	int halfer = first.size()/2; //k
 	vector<int> lowFirst = separate(first,halfer,true); //a0
 	vector<int> highFirst = separate(first,halfer,false); //a1
 	vector<int> lowSecond = separate(second,halfer,true); //b0
 	vector<int> highSecond = separate(second,halfer,false); //b1
-
+/*
+	cout << "LowFirst: ";
+	printNumber(lowFirst);
+	cout << " HighFirst: ";
+	printNumber(highFirst);
+	cout << " LowSecond: ";
+	printNumber(lowSecond);
+	cout << " HighSecond: ";
+	printNumber(highSecond);
+	cout << endl;
+*/
 	vector<int> section1 = recursiveKaratsuba(highFirst,highSecond,base); //p2
+	/*cout << "Number1: ";
+	printNumber(first);
+	cout << " Number2: ";
+	printNumber(second);
+	cout << " LowFirst: ";
+	printNumber(lowFirst);
+	cout << " HighFirst: ";
+	printNumber(highFirst);
+	cout << " LowSecond: ";
+	printNumber(lowSecond);
+	cout << " HighSecond: ";
+	printNumber(highSecond);
+	cout << " Section1 ";
+	printNumber(section1);
+	cout << endl;*/
 	vector<int> section3 = recursiveKaratsuba(lowFirst,lowSecond,base); //p0
+	/*cout << "Number1: ";
+	printNumber(first);
+	cout << " Number2: ";
+	printNumber(second);
+	cout << " LowFirst: ";
+	printNumber(lowFirst);
+	cout << " HighFirst: ";
+	printNumber(highFirst);
+	cout << " LowSecond: ";
+	printNumber(lowSecond);
+	cout << " HighSecond: ";
+	printNumber(highSecond);
+	cout << " Section3 ";
+	printNumber(section3);
+	cout << endl;*/
 	vector<int> section2Part1 = schoolAddition(highFirst,lowSecond,base);
+	/*cout << "Number1: ";
+	printNumber(first);
+	cout << " Number2: ";
+	printNumber(second);
+	cout << " LowFirst: ";
+	printNumber(lowFirst);
+	cout << " HighFirst: ";
+	printNumber(highFirst);
+	cout << " LowSecond: ";
+	printNumber(lowSecond);
+	cout << " HighSecond: ";
+	printNumber(highSecond);
+	cout << " Section2Part1 ";
+	printNumber(section2Part1);
+	cout << endl;*/
 	vector<int> section2Part2 = schoolAddition(lowFirst,highSecond,base);
+	/*cout << "Number1: ";
+	printNumber(first);
+	cout << " Number2: ";
+	printNumber(second);
+	cout << " LowFirst: ";
+	printNumber(lowFirst);
+	cout << " HighFirst: ";
+	printNumber(highFirst);
+	cout << " LowSecond: ";
+	printNumber(lowSecond);
+	cout << " HighSecond: ";
+	printNumber(highSecond);
+	cout << " Section2Part2 ";
+	printNumber(section2Part2);
+	cout << endl;*/
 	vector<int> section2 = recursiveKaratsuba(section2Part1,section2Part2,base); //p1
+	/*cout << "Number1: ";
+	printNumber(first);
+	cout << " Number2: ";
+	printNumber(second);
+	cout << " LowFirst: ";
+	printNumber(lowFirst);
+	cout << " HighFirst: ";
+	printNumber(highFirst);
+	cout << " LowSecond: ";
+	printNumber(lowSecond);
+	cout << " HighSecond: ";
+	printNumber(highSecond);
+	cout << " Section2 ";
+	printNumber(section2);
+	cout << endl;*/
 
 	vector<int> intermediate = schoolAddition(section1,section3,base); //p2+p0
+	/*cout << "Intermediate ";
+	printNumber(intermediate);
+	cout << endl;*/
 	intermediate = negative(intermediate);
 	intermediate = schoolAddition(intermediate,section2,base); //p1-(p2+p0)
+	/*cout << "Intermediate2 ";
+	printNumber(intermediate);
+	cout << endl;*/
 
 	//Pad 2k zeroes to the end of p2
 	for (int i = 0; i < 2*halfer; i++)
@@ -224,20 +340,30 @@ vector<int> recursiveKaratsuba (vector<int> first, vector<int> second, int base)
 		intermediate.push_back(0);
 	}
 
+	section1 = removeLeading(section1);
+	section3 = removeLeading(section3);
+	intermediate = removeLeading(intermediate);
+
 	vector<int> finale (1,0);
+	/*cout << "SECTION1 ";
+	printNumber(section1);
+	cout << " INTERMEDIATE ";
+	printNumber(intermediate);
+	cout << " SECTION3 ";
+	printNumber(section3);*/
 	finale = schoolAddition(finale,section1,base);
+	/*cout << " Finale1 ";
+	printNumber(finale);*/
 	finale = schoolAddition(finale,intermediate,base);
+	/*cout << " Finale2 ";
+	printNumber(finale);*/
 	finale = schoolAddition(finale,section3,base);
+	/*cout << " Finale3 ";
+	printNumber(finale);
+	cout << endl;*/
 
 	return finale;
 
-	/*
-	for (int i = 0; i < section1.size(); i++)
-	{
-		cout << section1[i];
-	}
-	cout << endl;
-	*/
 }
 
 
@@ -296,7 +422,7 @@ int main()
 	}
 
 	//School Addition
-	vector<int> sum = schoolAddition(firstNum,secondNum,baseNumber);
+	vector<int> sum = removeLeading( schoolAddition(firstNum,secondNum,baseNumber) );
 	for (int i = 0; i < sum.size(); i++)
 	{
 		cout << sum[i];
@@ -306,7 +432,7 @@ int main()
 
 
 	//Karatsuba Multiplication
-	vector<int> karatsubaResult = karatsubaMultiplication(firstNum,secondNum,baseNumber);
+	vector<int> karatsubaResult = removeLeading( karatsubaMultiplication(firstNum,secondNum,baseNumber) );
 
 
 	//Division for postgraduates only (NOT REQUIRED)
