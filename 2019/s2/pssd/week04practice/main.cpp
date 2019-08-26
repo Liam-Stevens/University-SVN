e46e4b5616a579a7ba13efe73c29d6df00f99141
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-08-26 08:09:48 +0000

    Week04 Q01 T03

--- 2019/s2/pssd/week04practice/main.cpp
+++ 2019/s2/pssd/week04practice/main.cpp
@@ -11,7 +11,7 @@ int main()
 	//vector<int> test32;
 	//test32.push_back(0);
 	MagicSpell test;
-	cout << test.fixTheSpell("AABZCADZA") << endl;
+	cout << test.fixTheSpell("ZZZAZZFAZAZBANZZZFZAZZZPZAAASZZAZZZ") << endl;
 
 
 	return 0;
