1200d474aaedb04f4be0da54f2679d9194aea649
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-08-26 08:05:05 +0000

    Week04 Q01 T02

--- 2019/s2/pssd/week04practice/MagicSpell.hpp
+++ 2019/s2/pssd/week04practice/MagicSpell.hpp
@@ -1,25 +1,59 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class MagicSpell
 {
+private:
+    string swap(string swapper, int element1, int element2)
+    {
+        char tmp = swapper[element1];
+        swapper[element1] = swapper[element2];
+        swapper[element2] = tmp;
+        return swapper;
+    }
+
 public:
     string fixTheSpell(string spell)
     {
         int elements = spell.length();
+        vector<int> firstHalf;
+        vector<int> secondHalf;
 
+        //Records locations of A and Z
         for(int i = 0; i < elements/2; i++)
         {
-            if(spell[i] == 'A' && spell[elements-1-i] == 'Z')
+            if(spell[i] == 'A' || spell[i] == 'Z')
             {
-                spell[i] = 'Z';
-                spell[elements-1-i] = 'A';
-            } else if(spell[i] == 'Z' && spell[elements-1-i] == 'A')
-            {
-                spell[i] = 'A';
-                spell[elements-1-i] = 'Z';
+                firstHalf.push_back(i);
             }
+        }
+        for(int i = elements-1; i >= elements/2; i--)
+        {
+            if(spell[i] == 'A' || spell[i] == 'Z')
+            {
+                secondHalf.push_back(i);
+            }
+        }
+
+        //Ensures that both halfs are the same size
+        while(firstHalf.size() != secondHalf.size())
+        {
+            if(firstHalf.size() > secondHalf.size())
+            {
+                firstHalf.erase(firstHalf.begin()+firstHalf.size());
+            }
+            if(firstHalf.size() < secondHalf.size())
+            {
+                secondHalf.erase(secondHalf.begin()+secondHalf.size());
+            }
+        }
+
+        //Swaps all elements
+        for(int i = 0; i < firstHalf.size(); i++)
+        {
+            spell = swap(spell,firstHalf[i],secondHalf[i]);
         }
 
         return spell;
