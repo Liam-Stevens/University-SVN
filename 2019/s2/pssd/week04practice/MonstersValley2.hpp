fe643bcdacd544f689b2899b12a6924f3a733b16
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-08-28 06:54:30 +0000

    Week04 Q03 T02

--- 2019/s2/pssd/week04practice/MonstersValley2.hpp
+++ 2019/s2/pssd/week04practice/MonstersValley2.hpp
@@ -13,6 +13,11 @@ public:
         int cost = 0;
         int power = 0;
 
+        if(dread.size() == 1 && dread[0] > 0)
+        {
+            cost = price[0];
+            return cost;
+        }
 
         for (int i = 0; i < dread.size(); i++)
         {
@@ -33,11 +38,11 @@ public:
                             }
                         }
 
-                        //cout << "Best Power " << bestPowerCost << endl;
+                        cout << "Best Power " << bestPowerCost << endl;
 
                         if (bestPowerCost > (float)dread[i]/(float)price[i])
                         {
-                            //cout << "[1] Price: " << powerCost[index] << " | Power Gained: " << freePower[index] << " | Current Power: " << power << endl;
+                            cout << "[1] Price: " << powerCost[index] << " | Power Gained: " << freePower[index] << " | Current Power: " << power << endl;
                             cost = cost + powerCost[index];
                             power = power + freePower[index];
                             powerCost.erase(powerCost.begin()+index);
@@ -46,13 +51,13 @@ public:
 
                             if (power + freePower[index] >= dread[i] && powerCost[index] < price[i])
                             {
-                                //cout << "[2] Price: " << powerCost[index] << " | Power Gained: " << freePower[index] << " | Current Power: " << power << endl;
+                                cout << "[2] Price: " << powerCost[index] << " | Power Gained: " << freePower[index] << " | Current Power: " << power << endl;
                                 cost = cost + powerCost[index];
                                 power = power + freePower[index];
                                 powerCost.erase(powerCost.begin()+index);
                                 freePower.erase(freePower.begin()+index);
                             } else {
-                                //cout << "[3] Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
+                                cout << "[3] Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
                                 cost = cost + price[i];
                                 power = power + dread[i];
                                 freePower.clear();
@@ -60,7 +65,7 @@ public:
                             }
 
                         } else {
-                            //cout << "[4] Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
+                            cout << "[4] Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
                             cost = cost + price[i];
                             power = power + dread[i];
                             freePower.clear();
@@ -70,13 +75,13 @@ public:
 
                 } else {
 
-                    //cout << "Nothing Avaliable | Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
+                    cout << "Nothing Avaliable | Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
                     cost = cost + price[i];
                     power = power + dread[i];
                 }
 
             } else {
-                //cout << "Not Needed | Beat: " << dread[i] << " | Current Power: " << power << endl;
+                cout << "Not Needed | Beat: " << dread[i] << " | Current Power: " << power << endl;
                 freePower.push_back(dread[i]);
                 powerCost.push_back(price[i]);
             }
