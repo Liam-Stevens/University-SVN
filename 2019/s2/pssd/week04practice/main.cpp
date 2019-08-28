8abf78032c32c5b6d1b0aa57e3602bcea334116f
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-08-28 09:44:52 +0000

    Week04 Q03 T10

--- 2019/s2/pssd/week04practice/main.cpp
+++ 2019/s2/pssd/week04practice/main.cpp
@@ -9,30 +9,16 @@ using namespace std;
 int main()
 {
 	vector<int> test32;
-	test32.push_back(1252017);
-	test32.push_back(3776329);
-	test32.push_back(21140492);
-	test32.push_back(5357875);
-	test32.push_back(59227052);
-	test32.push_back(88302560);
-	test32.push_back(344236822);
-	test32.push_back(52938928);
-	test32.push_back(256182128);
-	test32.push_back(321584565);
-	test32.push_back(22692247);
+	test32.push_back(3);
+	test32.push_back(2);
+	test32.push_back(5);
+	test32.push_back(7);
 
 	vector<int> test33;
-	test33.push_back(2);
-	test33.push_back(2);
-	test33.push_back(1);
 	test33.push_back(1);
 	test33.push_back(1);
 	test33.push_back(2);
 	test33.push_back(2);
-	test33.push_back(1);
-	test33.push_back(2);
-	test33.push_back(2);
-	test33.push_back(1);
 
 	MonstersValley2 test;
 	cout << test.minimumPrice(test32,test33) << endl;
