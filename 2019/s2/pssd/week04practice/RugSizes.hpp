23a4aab1d067d62160e6ea559cdb99f18016ca6a
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-08-28 06:01:06 +0000

    Week04 Q02 T03

--- 2019/s2/pssd/week04practice/RugSizes.hpp
+++ 2019/s2/pssd/week04practice/RugSizes.hpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class RugSizes
 {
+private:
+    vector<int> found;
+    bool unique(int num)
+    {
+        if(found.size() == 0) {
+            return true;
+        } else {
+            for(int i = 0; i < found.size(); i++)
+            {
+                if(found[i] == num)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 public:
     int rugCount(int area)
     {
         int sizes = 0;
+        found.clear();
 
         for (int i = 1; i <= area/2; i++)
         {
@@ -16,11 +36,17 @@ public:
                 if(i == test)
                 {
                     sizes++;
+                    //cout << i << " | " << test << endl;
                 }
             } else {
                 if (test*i == area)
                 {
-                    sizes++;
+                    if(unique(test) == true)
+                    {
+                        found.push_back(i);
+                        sizes++;
+                        //cout << i << " | " << test << endl;
+                    }
                 }
             }
         }
