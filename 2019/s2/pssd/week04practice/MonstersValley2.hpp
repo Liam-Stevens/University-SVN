fa3c52fd023719d646c980da58b0c4a89ebcfa4f
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-08-28 09:44:52 +0000

    Week04 Q03 T10

--- 2019/s2/pssd/week04practice/MonstersValley2.hpp
+++ 2019/s2/pssd/week04practice/MonstersValley2.hpp
@@ -75,7 +75,7 @@ public:
 
                         if (bestPowerCost > (long double)dread[i]/(long double)price[i])
                         {
-                            //cout << "[1] Price: " << powerCost[index] << " | Power Gained: " << freePower[index] << " | Current Power: " << power << endl;
+                            cout << "[1] Price: " << powerCost[index] << " | Power Gained: " << freePower[index] << " | Current Power: " << power << endl;
                             cost = cost + powerCost[index];
                             power = power + freePower[index];
                             powerCost.erase(powerCost.begin()+index);
@@ -86,25 +86,25 @@ public:
                             {
                                 if (powerCheck(i,index,power,cost,dread,price))
                                 {
-                                    //cout << "[2] Price: " << powerCost[index] << " | Power Gained: " << freePower[index] << " | Current Power: " << power << endl;
+                                    cout << "[2] Price: " << powerCost[index] << " | Power Gained: " << freePower[index] << " | Current Power: " << power << endl;
                                     cost = cost + powerCost[index];
                                     power = power + freePower[index];
                                     powerCost.erase(powerCost.begin()+index);
                                     freePower.erase(freePower.begin()+index);
                                 } else {
-                                    //cout << "[3] Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
+                                    cout << "[3] Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
                                     cost = cost + price[i];
                                     power = power + dread[i];
                                 }
                             } else {
-                                //cout << "[4] Price: " << powerCost[index] << " | Power Gained: " << freePower[index] << " | Current Power: " << power << endl;
+                                cout << "[4] Price: " << powerCost[index] << " | Power Gained: " << freePower[index] << " | Current Power: " << power << endl;
                                 cost = cost + powerCost[index];
                                 power = power + freePower[index];
                                 powerCost.erase(powerCost.begin()+index);
                                 freePower.erase(freePower.begin()+index);
                             }
                         } else {
-                            //cout << "[5] Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
+                            cout << "[5] Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
                             cost = cost + price[i];
                             power = power + dread[i];
                         }
@@ -112,13 +112,13 @@ public:
 
                 } else {
 
-                    //cout << "Nothing Avaliable | Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
+                    cout << "Nothing Avaliable | Price: " << price[i] << " | Power Gained: " << dread[i] << " | Current Power: " << power << endl;
                     cost = cost + price[i];
                     power = power + dread[i];
                 }
 
             } else {
-                //cout << "Not Needed | Stored: " << dread[i] << " | Current Power: " << power << endl;
+                cout << "Not Needed | Stored: " << dread[i] << " | Current Power: " << power << endl;
                 freePower.push_back(dread[i]);
                 powerCost.push_back(price[i]);
             }
