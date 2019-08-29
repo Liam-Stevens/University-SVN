a33b60aea40ba72c7f9668a9d7660073f1709230
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-08-29 03:58:07 +0000

    testing

---
+++ 2019/s2/pssd/week05practice/main.cpp
@@ -0,0 +1,16 @@
+#include "UnsealTheSafe.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctype.h>
+
+using namespace std;
+
+int main()
+{
+	UnsealTheSafe test;
+	cout << test.countPasswords(3) << endl;
+
+
+	return 0;
+}
