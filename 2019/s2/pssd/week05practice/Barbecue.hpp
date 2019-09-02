ff1fc45b91555f56124d13aff8d8a23d63781d81
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-09-02 05:42:37 +0000

    Week05 Q02 T01

--- 2019/s2/pssd/week05practice/Barbecue.hpp
+++ 2019/s2/pssd/week05practice/Barbecue.hpp
@@ -7,38 +7,76 @@ class Barbecue
 {
 public:
     int eliminate(int n, vector<int> voter, vector<int> excluded)
-    {      
+    {
         int elements = voter.size();
-        vector<int> votes(0,n);
-        vector<int> exile(0,n);
+        vector<int> votes(n,0);
+        vector<int> exile(n,0);
 
         for(int i = 0; i < elements; i++)
         {
-            //votes[voter[i]]++;
-            //exile[excluded[i]]++;
+            votes[voter[i]]++;
+            exile[excluded[i]]++;
         }
-        cout << "test" << endl;
         bool stalemate = false;
         int max = 0;
         for (int i = 0; i < elements; i++)
         {
-            /*if(max < exile[i])
+            if(max < exile[i])
             {
                 stalemate = false;
                 max = exile[i];
             } else if (max == exile[i])
             {
                 stalemate == true;
-            }*/
+            }
         }
 
         if(stalemate == true)
         {
-            cout << "stalemate" << endl;
+            stalemate = false;
+            int maxVotes = 0;
+            int maxVoter = 0;
+            vector<int> stalemates;
+
+            for (int i = 0; i < votes.size(); i++)
+            {
+                if (votes[i] > maxVotes)
+                {
+                    maxVoter = i;
+                    maxVotes = votes[i];
+                    stalemates.clear();
+                    stalemates.push_back(i);
+                }
+            }
+            for (int i = 0; i < votes.size(); i++)
+            {
+                if (votes[i] == maxVotes && i != maxVoter)
+                {
+                    stalemate = true;
+                    stalemates.push_back(i);
+                }
+            }
+
+            if(stalemate == true)
+            {
+                int minIndex = stalemates[0];
+                for (int i = 0; i < stalemates.size(); i++)
+                {
+                    if (minIndex > stalemates[i])
+                    {
+                        minIndex = stalemates[i];
+                    }
+                }
+                return minIndex;
+            } else {
+                return maxVoter;
+            }
+
+
         } else {
             return max;
         }
 
-        return 0;
+        return -1;
     }
 };
