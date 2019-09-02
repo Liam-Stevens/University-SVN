6572119c499ea51f9d2055f163f480921e153bad
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-09-02 05:50:12 +0000

    Week05 Q02 T02

--- 2019/s2/pssd/week05practice/main.cpp
+++ 2019/s2/pssd/week05practice/main.cpp
@@ -10,12 +10,10 @@ int main()
 {
 	vector<int> test32;
 	test32.push_back(0);
-	test32.push_back(1);
 	test32.push_back(2);
 	test32.push_back(2);
 
 	vector<int> test33;
-	test33.push_back(2);
 	test33.push_back(2);
 	test33.push_back(0);
 	test33.push_back(1);
