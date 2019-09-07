204c5410ae2f934bfe105a132f742cd43cb05d31
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-09-07 07:48:56 +0000

    Week06 Q04 T01

---
+++ 2019/s2/pssd/week06practice/NiceOrUgly.hpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class NiceOrUgly
+{
+private:
+    bool checkVowel(char letter)
+    {
+        if (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U')
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool checkConsonent(char letter)
+    {
+        if (letter != 'A' && letter != 'E' && letter != 'I' && letter != 'O' && letter != 'U' && letter != '?')
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+public:
+    string describe(string s)
+    {
+        int vowel = 0;
+        int consonant = 0;
+        bool defVowel = false;
+        bool defConsonant = false;
+        bool vowelAvaliable = false;
+        bool consonantAvaliable = false;
+        bool marked = false;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if(checkVowel(s[i]))
+            {
+                vowel++;
+                if(consonant > 0)
+                {
+                    consonant = 0;
+                    consonantAvaliable = false;
+                }
+            } else if (checkConsonent(s[i]))
+            {
+                consonant++;
+                if(vowel > 0)
+                {
+                    vowel = 0;
+                    vowelAvaliable = false;
+                }
+            } else if (s[i] == '?')
+            {
+                vowel++;
+                consonant++;
+                vowelAvaliable = true;
+                consonantAvaliable = true;
+            }
+
+            //cout << s[i] << " V: " << vowel << " C: " << consonant << " ?1 " << vowelAvaliable << " ?2 " << consonantAvaliable << endl;
+
+            if(vowel >= 3)
+            {
+                defVowel = true;
+                if(vowelAvaliable == true)
+                {
+                    marked = true;
+                }
+                //cout << "FOUND VOWEL" << endl;
+            }
+            if(consonant >= 5)
+            {
+                defConsonant = true;
+                if(consonantAvaliable == true)
+                {
+                    marked = true;
+                }
+                //cout << "FOUND CONSONANT" << endl;
+            }
+        }
+
+        if(marked == true)
+        {
+            return "42";
+        } else if (defVowel == true || defConsonant == true)
+        {
+            return "UGLY";
+        } else
+        {
+            return "NICE";
+        }
+
+        return "ERROR";
+    }
+};
