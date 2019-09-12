6a5c5bab4773f696f6defa8da619cb6f459c26fb
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-09-12 03:54:31 +0000

    Week07 Q02 T01

---
+++ 2019/s2/pssd/week07practice/MinimalDifference.hpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+using namespace std;
+
+class MinimalDifference
+{
+private:
+    int digitSum(int num)
+    {
+        int sum = 0;
+        while(num > 0)
+        {
+            sum = sum + num%10;
+            num = num / 10;
+        }
+        return sum;
+    }
+
+public:
+    int findNumber(int A, int B, int C)
+    {
+        C = digitSum(C);
+
+        int minDiff = B - A;
+        int currentNum = 0;
+
+        for(int i = A; i <= B; i++)
+        {
+            int tmp = abs(C-digitSum(i));
+            if (tmp < minDiff)
+            {
+                minDiff = tmp;
+                currentNum = i;
+            }
+
+            
+        }
+
+        return currentNum;
+    }
+};
