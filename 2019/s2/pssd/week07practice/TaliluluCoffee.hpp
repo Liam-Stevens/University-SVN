ba72dc7e4d0818f1d530f62ca97b8a20980588fb
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-09-27 11:24:51 +0000

    Week07 Comments

--- 2019/s2/pssd/week07practice/TaliluluCoffee.hpp
+++ 2019/s2/pssd/week07practice/TaliluluCoffee.hpp
@@ -8,42 +8,20 @@ class TaliluluCoffee
 private:
     vector<int> customers;
 
-    int findLow()
+    int findMax()
     {
-        bool foundOne = false;
-        bool large = false;
-        int min = 999999;
         int index = -1;
-        int indexOne = -1;
-
         int max = 0;
 
         for (int i = 0; i < customers.size(); i++)
         {
-            /*if (customers[i] > 1 && min > customers[i])
-            {
-                large = true;
-                index = i;
-                min = customers[i];
-            } else if (customers[i] == 1)
-            {
-                foundOne = true;
-                indexOne = i;
-            }*/
+
             if (max < customers[i])
             {
                 max = customers[i];
                 index = i;
             }
         }
-/*
-        if (large == false && foundOne == true)
-        {
-            index = indexOne;
-        } else if (large == false && foundOne == false)
-        {
-            index = 0;
-        }*/
 
         return index;
     }
@@ -57,9 +35,7 @@ private:
             {
                 customers[i] = 0;
             }
-            //cout << customers[i] << " ";
         }
-        //cout << endl;
     }
 
 public:
@@ -71,10 +47,10 @@ public:
         for (int i = 0; i < tips.size(); i++)
         {
 
-            int index = findLow();
-            //cout << "PICKED: " << customers[index] << endl;
+            int index = findMax();
+
             money += customers[index];
-            //cout << "MONEY = " << money << endl << endl;
+            
             customers.erase(customers.begin() + index);
 
             reduceTips();
