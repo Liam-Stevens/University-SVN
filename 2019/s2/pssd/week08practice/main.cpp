af5c66c224c15823b4875983c73d9e62c8162673
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-09-30 10:37:50 +0000

    Week08 Q01 T01

--- 2019/s2/pssd/week08practice/main.cpp
+++ 2019/s2/pssd/week08practice/main.cpp
@@ -1,4 +1,4 @@
-#include "TaliluluCoffee.hpp"
+#include "Thimbles.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,31 +9,16 @@ using namespace std;
 int main()
 {
 
-	vector<int> test32;
-	test32.push_back(27);
-	test32.push_back(24);
-	test32.push_back(48);
-	test32.push_back(27);
-	test32.push_back(10);
-	test32.push_back(20);
-	test32.push_back(35);
-	test32.push_back(22);
-	test32.push_back(27);
-	test32.push_back(26);
-	test32.push_back(4);
-	test32.push_back(6);
-	test32.push_back(32);
-	test32.push_back(19);
-	test32.push_back(24);
-	test32.push_back(37);
-	test32.push_back(43);
+	vector<string> test32;
+	test32.push_back("1-2");
+	test32.push_back("3-1");
 	/*
 	vector<int> test33;
 	test33.push_back(4);
 	test33.push_back(6);*/
 
-	TaliluluCoffee test;
-	cout << test.maxTip(test32) << endl;
+	Thimbles test;
+	cout << test.thimbleWithBall(test32) << endl;
 
 
 	return 0;
