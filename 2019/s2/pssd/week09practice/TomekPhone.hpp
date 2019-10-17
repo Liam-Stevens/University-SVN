e0ad1564a65a001412d7b7cbb7ae15119ffd2e61
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-10-17 08:17:55 +0000

    Week09 Q04 T01

---
+++ 2019/s2/pssd/week09practice/TomekPhone.hpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+class TomekPhone
+{
+private:
+
+    int sum(vector<int> input)
+    {
+        int sum = 0;
+        for (int i = 0; i < input.size(); i++)
+        {
+            sum = sum + input[i];
+        }
+        return sum;
+    }
+
+public:
+    int minKeystrokes(vector<int> frequencies, vector<int> keySizes)
+    {
+        if (sum(keySizes) < frequencies.size())
+        {
+            //cout << sum(keySizes) << " | " << frequencies.size() << endl;
+            return -1;
+        }
+
+        int strokes = 0;
+        int depth = 1;
+        int head = 0;
+
+        sort(frequencies.begin(),frequencies.end(), greater<int>());
+
+        for (int i = 0; i < frequencies.size(); i++)
+        {
+            while (keySizes[head] < depth)
+            {
+                head++;
+                if (head+1 > keySizes.size())
+                {
+                    depth++;
+                    head = 0;
+                }
+            }
+            strokes = strokes + frequencies[i]*depth;
+            head++;
+            if (head+1 > keySizes.size())
+            {
+                depth++;
+                head = 0;
+            }
+        }
+
+        return strokes;
+    }
+};
