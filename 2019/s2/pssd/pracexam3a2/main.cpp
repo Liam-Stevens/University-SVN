b07c2e40646bcad91dcee4bcabce26cfa21f5de5
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2019-10-31 03:09:22 +0000

    PracExam3 Q02 T04

--- 2019/s2/pssd/pracexam3a2/main.cpp
+++ 2019/s2/pssd/pracexam3a2/main.cpp
@@ -9,18 +9,13 @@ using namespace std;
 int main()
 {
 	vector<string> test32;
-	test32.push_back("A");
-	test32.push_back("quick");
-	test32.push_back("brown");
-	test32.push_back("fox");
-	test32.push_back("jumps");
-	test32.push_back("over");
-	test32.push_back("the");
-	test32.push_back("lazy");
-	test32.push_back("dog");
+	test32.push_back("Alpha");
+	test32.push_back("Beta");
+	test32.push_back("John");
+	test32.push_back("said");
 
 	LineSpacing test;
-	cout << test.adjusted(test32, 50) << endl;
+	cout << test.adjusted(test32, 29) << endl;
 
 
 	return 0;
