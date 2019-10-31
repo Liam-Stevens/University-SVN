606a4e3f37c662cd9598e939e2106787282330dd
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-10-31 02:14:02 +0000

    PracExam3 Q01 T01

---
+++ 2019/s2/pssd/pracexam3a1/main.cpp
@@ -0,0 +1,17 @@
+#include "WordGame.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctype.h>
+
+using namespace std;
+
+int main()
+{
+
+	WordGame test;
+	cout << test.bestScore("TOPCODERROCKS") << endl;
+
+
+	return 0;
+}
