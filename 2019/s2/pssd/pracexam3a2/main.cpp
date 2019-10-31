58b05dea1efc96d4e2a1de1eae9935b8fc0c7eaa
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2019-10-31 03:12:06 +0000

    PracExam3 Q02 Removed Redundency

--- 2019/s2/pssd/pracexam3a2/main.cpp
+++ 2019/s2/pssd/pracexam3a2/main.cpp
@@ -11,11 +11,12 @@ int main()
 	vector<string> test32;
 	test32.push_back("Alpha");
 	test32.push_back("Beta");
-	test32.push_back("John");
-	test32.push_back("said");
+	test32.push_back("Gamma");
+	test32.push_back("Delta");
+	test32.push_back("Epsilon");
 
 	LineSpacing test;
-	cout << test.adjusted(test32, 29) << endl;
+	cout << test.adjusted(test32, 32) << endl;
 
 
 	return 0;
