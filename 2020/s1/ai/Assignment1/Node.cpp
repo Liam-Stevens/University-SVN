354a5f23c2f5942bc04041f1b1e0f2c616e9657e
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2020-03-27 04:24:43 +0000

    Initial State Test

--- 2020/s1/ai/Assignment1/Node.cpp
+++ 2020/s1/ai/Assignment1/Node.cpp
@@ -1,15 +1,18 @@
 #include "Node.h"
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 Node::Node()
 {
-	//Does Nothing
+	path = false;
 }
 
 Node::Node(int newData)
 {
 	elevation = newData;
+	path = false;
 }
 
 //Getters
@@ -38,6 +41,34 @@ Node * Node::getRight()
 	return right;
 }
 
+int Node::getX()
+{
+	return posX;
+}
+
+int Node::getY()
+{
+	return posY;
+}
+
+Node * Node::getTrace()
+{
+	if (previousNodes.size() == 0)
+	{
+		return NULL;
+	}
+	else
+	{
+		return previousNodes[0];
+	}
+}
+
+bool Node::getPath()
+{
+	return path;
+}
+
+
 //Setters
 void Node::setElevation(int newData)
 {
@@ -62,4 +93,68 @@ void Node::setLeft(Node * newPtr)
 void Node::setRight(Node * newPtr)
 {
 	right = newPtr;
+}
+
+void Node::setPos(int setX, int setY)
+{
+	posX = setX;
+	posY = setY;
+}
+
+void Node::setPath(bool newPath)
+{
+	path = newPath;
+}
+
+
+//Traceback
+void Node::insertNode(Node * newNode)
+{
+	queuedNodes.push_back(newNode);
+}
+
+void Node::printNodes()
+{
+	for (int i = 0; i < (signed)previousNodes.size(); i++)
+	{
+		cout << "["<< previousNodes[i]->getX()+1 << "]["<< previousNodes[i]->getY()+1 << "], ";
+	}
+}
+
+void Node::visited(bool visit)
+{
+	if (visit == false)
+	{
+		queuedNodes.erase(queuedNodes.begin());
+	}
+	else
+	{
+		if (queuedNodes.size() == 0)
+		{
+			//Starting Location
+		}
+		else
+		{
+			previousNodes.push_back(queuedNodes[0]);
+			queuedNodes.erase(queuedNodes.begin());
+		}
+	}
+}
+
+void Node::traceNodes()
+{
+	Node * currentNode = previousNodes[0];
+	path = true;
+
+	while(true)
+	{
+		if (currentNode == NULL)
+		{
+			break;
+		}
+
+		currentNode->setPath(true);
+		currentNode = currentNode->getTrace();
+	}
+
 }
