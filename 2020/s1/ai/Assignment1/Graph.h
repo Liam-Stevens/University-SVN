9643ab3705c439b3482df71c5b89b14819860765
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2020-03-27 06:52:27 +0000

    USC search 01

--- 2020/s1/ai/Assignment1/Graph.h
+++ 2020/s1/ai/Assignment1/Graph.h
@@ -21,12 +21,14 @@ public:
 
 	//Helpers
 	bool inSet(Node *, std::vector<Node *>);
+	void priorityQueue(Node *, int, std::vector<Node *> *, std::vector<int> *);
 	std::vector<Node *> expandNode(Node *, std::vector<Node *>);
+	void expandNodeCost(Node *, int, std::vector<Node *> *, std::vector<int> *);
 
 	//Search Algorithms
-	void BFS(int, int, int, int);
-	void UCS();
-	void ASTAR(bool);
+	bool BFS(int, int, int, int);
+	bool UCS(int, int, int, int);
+	bool ASTAR(bool);
 
 	//Destructor
 	~Graph();
