d91a3dd93fccd9bc475d11e7913c5cc610ff7b7e
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2020-03-29 05:32:46 +0000

    ASTAR implemented

--- 2020/s1/ai/Assignment1/Graph.h
+++ 2020/s1/ai/Assignment1/Graph.h
@@ -19,16 +19,21 @@ public:
 	void setNodeConnections();
 	void printGraph(bool);
 
+	//Heuristics
+	double calcEucladean(Node *, Node *);
+	double calcManhattan(Node *, Node *);
+
 	//Helpers
 	bool inSet(Node *, std::vector<Node *>);
 	void priorityQueue(Node *, int, std::vector<Node *> *, std::vector<int> *);
 	std::vector<Node *> expandNode(Node *, std::vector<Node *>);
 	void expandNodeCost(Node *, int, std::vector<Node *> *, std::vector<int> *);
+	void expandNodeAStar(bool, Node *, Node *, int, std::vector<Node *> *, std::vector<int> *);
 
 	//Search Algorithms
 	bool BFS(int, int, int, int);
 	bool UCS(int, int, int, int);
-	bool ASTAR(bool);
+	bool ASTAR(bool, int, int, int, int);
 
 	//Destructor
 	~Graph();
