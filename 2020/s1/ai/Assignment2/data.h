ec3d7210f61b56a597613cd4039b65e1c3446a37
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2020-04-28 08:38:09 +0000

    AI Ass2 Read In Data

---
+++ 2020/s1/ai/Assignment2/data.h
@@ -0,0 +1,12 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <vector>
+#include <string>
+
+struct data {
+    std::vector<std::string> ratings;
+	std::vector< std::vector<float> > attributes;
+};
+
+#endif
