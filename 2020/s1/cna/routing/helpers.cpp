6deba4baba0a89c3c1608f5a6326f5ff2c99275c
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2020-05-27 06:52:20 +0000

    read in data from files

--- 2020/s1/cna/routing/helpers.cpp
+++ 2020/s1/cna/routing/helpers.cpp
@@ -4,21 +4,75 @@
 #include <sstream>
 #include <vector>
 #include "helpers.h"
+#include "data.h"
 
 using namespace std;
 
 /*-----------------------------------------
 | Verifies the input arguments
 ------------------------------------------*/
-bool verify()
+bool verifyArgs(int argNum, string configLocation, string changedConfigLocation)
 {
+	//Check minleaf validity
+	if (argNum != 3)
+	{
+		return 1;
+	}
 
+    //TODO: Check location validity (Unsure if needed)
+
+	return 0;
 }
 
 /*-----------------------------------------
 | Read the file to a vector
 ------------------------------------------*/
-bool readData()
+bool readData(string dataLocation, struct data * myData, bool changed)
 {
+    string line;
+	ifstream dataFile (dataLocation.c_str());
 
+	//Open the file
+  	if (dataFile.is_open())
+  	{
+        stringstream lineStream;
+
+        //Read in node names
+        if ( !changed && getline (dataFile,line) )
+        {
+            int numOfNodes;
+            lineStream.str(line);
+            lineStream >> numOfNodes;
+
+        	for (int i = 0; i < numOfNodes; i++)
+        	{
+                getline (dataFile,line);
+
+                myData->keys.push_back(line);
+        	}
+        }
+
+        //Read in node connections
+        if ( getline (dataFile,line) )
+        {
+            int numOfConnections;
+            lineStream.str(line);
+            lineStream >> numOfConnections;
+
+            for (int i = 0; i < numOfConnections; i++)
+        	{
+                getline (dataFile,line);
+
+                myData->weights.push_back(line);
+        	}
+        }
+
+    	dataFile.close();
+  	}
+	//Unable to open the file
+  	else {
+		return 1;
+	}
+
+	return 0;
 }
