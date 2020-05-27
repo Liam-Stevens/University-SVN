2a3c654e759968a3fe063140aae4a4ad35a250d3
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2020-05-27 06:00:59 +0000

    starting point

---
+++ 2020/s1/cna/routing/helpers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "helpers.h"
+
+using namespace std;
+
+/*-----------------------------------------
+| Verifies the input arguments
+------------------------------------------*/
+bool verify()
+{
+
+}
+
+/*-----------------------------------------
+| Read the file to a vector
+------------------------------------------*/
+bool readData()
+{
+
+}
