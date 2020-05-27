a831110459bfce1f41d64e48a30b350beb915f69
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2020-05-27 06:52:20 +0000

    read in data from files

--- 2020/s1/cna/routing/helpers.h
+++ 2020/s1/cna/routing/helpers.h
@@ -1,8 +1,11 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
-extern bool verify();
+#include <string>
+#include "data.h"
 
-extern bool readData();
+extern bool verifyArgs(int, std::string, std::string);
+
+extern bool readData(std::string, struct data *, bool);
 
 #endif
--- 2020/s1/cna/routing/main.cpp
+++ 2020/s1/cna/routing/main.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "helpers.h"
+#include "data.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	
+	string configLocation = argv[1], changedConfigLocation = argv[2];
+
+	//Check argument validity
+	if ( verifyArgs(argc, configLocation, changedConfigLocation) )
+	{
+		cout << "Bad or missing arguments" << endl;
+		return 1;
+	}
+
+	struct data configData;
+	struct data changedConfigData;
+
+	//Read in Config Data
+	if ( readData(configLocation, &configData, false) )
+	{
+		cout << "Cannot Read Config Data" << endl;
+		return 1;
+	}
+
+	//Read in Changed Config Data
+	if ( readData(changedConfigLocation, &changedConfigData, true) )
+	{
+		cout << "Cannot Read Changed Config Data" << endl;
+		return 1;
+	}
+
+	for (int i = 0; i < (signed)configData.keys.size(); i++)
+	{
+		cout << configData.keys[i] << endl;
+	}
 
 	return 0;
 }
