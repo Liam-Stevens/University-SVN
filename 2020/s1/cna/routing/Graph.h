89783dc4e346f6842e2424a30c96c1ceba0bfcc6
Liam Stevens <80184825+Liam-Stevens@users.noreply.github.com>
2020-05-30 06:58:13 +0000

    timeStep0 registered

--- 2020/s1/cna/routing/Graph.h
+++ 2020/s1/cna/routing/Graph.h
@@ -9,19 +9,25 @@ class Graph
 {
 private:
 	std::vector< Node * > edges;
+	struct data setup;
+	struct data change;
 
 public:
 	//Constructor
     Graph(struct data, struct data);
 
 	void createConnection(int, int, int);
+	bool updateNodes();
 
 	void runDistanceVector();
 	bool nodeDistanceVector();
 
+	void outputTableChange(int, int, int, int, int);
+
 	//Debug
 	void printNodeKeys();
 	void printNodeConnections();
+	void printAllTables();
 
 	//Destructor
 	~Graph();
