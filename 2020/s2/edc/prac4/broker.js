a981b2ae9b918dbe5a32a55e3fb35776ea94e863
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2020-10-06 07:23:07 +0000

    complete implementation of ex4

--- 2020/s2/edc/prac4/broker.js
+++ 2020/s2/edc/prac4/broker.js
@@ -1,20 +1,12 @@
+/*
+    SUBSCRIPTION OBJECT FORMAT
+    Name: string
+    Handlers: [functions]
+
+*/
 var subscriptions = [];
 
-function emit(type)
-{
-    //console.log(subscriptions)
-    for (let [index, subscription] of subscriptions.entries()) {
-        if (subscription.name === type)
-        {
-            //console.log(subscription.name + " | " + type);
-            subscription.handlers.forEach( function (runHandler)
-            {
-                runHandler();
-            });
-        }
-    }
-}
-
+//Fetches the subscription with the given name
 function findSub(type)
 {
     for (let [index, subscription] of subscriptions.entries()) {
@@ -26,28 +18,81 @@ function findSub(type)
     return null;
 }
 
+//Finds if a handler already exists within the subscription
+function handlerExists(subscription, newHandler)
+{
+    for (let [index, currentHandler] of subscription.handlers.entries()) {
+        if (currentHandler === newHandler)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+//Subscripe a handler to an event
 function subscribe(type,handler)
 {
     var subscription = findSub(type);
-    if (subscription == null)
+
+    //Create new subscription if one does not exist
+    if (subscription === null)
     {
         subscription = { name: type, handlers: [] };
         subscriptions.push(subscription);
     }
-    subscription.handlers.push(handler);
+
+    //Do not add if handler already exists
+    if( !handlerExists(subscription, handler) )
+    {
+        subscription.handlers.push(handler);
+    }
+
 }
 
-function unsubscribe(type,handler)
+//Runs all handlers for the given event
+function emit(type)
 {
     var subscription = findSub(type);
-    if (subscription == null)
+    if (subscription === null)
     {
         return;
     }
 
-    //TODO: splice the handler out
+    //Runs all handlers in the subscription
+    subscription.handlers.forEach( function (runHandler)
+    {
+        runHandler();
+    });
+}
 
-    //TODO: if the handler array is empty slice the event out
+//Unsubscribe a handler from an event
+function unsubscribe(type,handler)
+{
+    var subscription = findSub(type);
+    if (subscription === null)
+    {
+        return;
+    }
+
+    //Splice the handler out
+    for (let [index, currentHandler] of subscription.handlers.entries()) {
+        if (currentHandler === handler)
+        {
+            subscription.handlers.splice(index, 1);
+        }
+    }
+
+    //If the handler array is empty splice the event out
+    if (subscription.handlers.length === 0)
+    {
+        for (let [index, currentSubscription] of subscriptions.entries()) {
+            if (currentSubscription === subscription)
+            {
+                subscriptions.splice(index, 1);
+            }
+        }
+    }
 }
 
 module.exports = { emit, subscribe, unsubscribe };
