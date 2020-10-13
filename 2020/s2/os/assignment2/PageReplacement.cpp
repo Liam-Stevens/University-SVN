c3a7e3ff723cc3c5f5ccedba39a45269dbcb5460
a1742143 <a1742143@ef3163f2-9ed4-4377-87a3-976858b64f63>
2020-10-13 07:13:49 +0000

    starting input

---
+++ 2020/s2/os/assignment2/PageReplacement.cpp
@@ -0,0 +1,32 @@
+/*
+Created By: Liam Stevens
+ID: a1742143
+Time:05.09.2020
+Contact Email: a1742143@student.adelaide.edu.au
+*/
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include "helpers.cpp"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    string fileLocation = argv[1];
+    vector<string> fileLines;
+
+    if ( readFile(fileLocation, &fileLines) )
+    {
+        cout << "FAILED AT READ" << endl;
+        return 1;
+    }
+
+    fileLines = filterComments(fileLines);
+
+    printStrings(fileLines);
+
+    return 0;
+}
---
+++ 2020/s2/os/assignment2/helpers.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+//Read a text file into a vector
+bool readFile(string fileLocation, vector<string> * fileLines)
+{
+    string line;
+  	ifstream inputFile (fileLocation.c_str());
+
+	//Open the file
+  	if (inputFile.is_open())
+  	{
+    	while ( getline (inputFile,line) )
+    	{
+      		fileLines->push_back(line);
+    	}
+    	inputFile.close();
+  	}
+	//Unable to open the file
+  	else {
+		return 1;
+	}
+    return 0;
+}
+
+vector<string> filterComments(vector<string> fileLines)
+{
+    vector<string> newLines;
+    for (int i = 0; i < (signed)fileLines.size(); i++)
+    {
+        if (fileLines[i][0] != '#')
+        {
+            newLines.push_back(fileLines[i]);
+        }
+    }
+    return newLines;
+}
+
+void printStrings(vector<string> lines)
+{
+    cout << "Printed Lines: " << endl;
+    for (int i = 0; i < (signed)lines.size(); i++)
+    {
+        cout << i << ": " << lines[i] << endl;
+    }
+}
